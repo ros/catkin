03c27825a38c043ac0e43845a3b753a561ce4881
Troy D. Straszheim <straszheim@willowgarage.com>
2011-11-09 18:47:44 -0800

    more

--- cmake/debian-util.cmake
+++ cmake/debian-util.cmake
@@ -3,6 +3,11 @@ execute_process(COMMAND date +%F-%0k-%0M-%0S%z
   OUTPUT_VARIABLE DEBIAN_SNAPSHOT_SUFFIX
   OUTPUT_STRIP_TRAILING_WHITESPACE)
 
+if (NOT DEBS_TARGET_ADDED)
+  add_custom_target(debs)
+  set(DEBS_TARGET_ADDED TRUE CACHE INTERNAL "onetime")
+endif()
+
 function(catkin_package PKGNAME)
   configure_file(${CMAKE_CURRENT_SOURCE_DIR}/debian/control
     ${CMAKE_CURRENT_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/debian/control.stamp
--- toplevel.cmake
+++ toplevel.cmake
@@ -3,7 +3,6 @@ cmake_minimum_required(VERSION 2.8)
 set(CMAKE_PREFIX_PATH ${CMAKE_CURRENT_BINARY_DIR})
 set(CATKIN_LANGS cpp)
 # set(CATKIN_LOG 9)
-add_custom_target(debs)
 add_subdirectory(catkin)
 add_subdirectory(genmsg)
 
