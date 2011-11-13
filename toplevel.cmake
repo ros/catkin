6f08175c34d5f1865c47c7cc5af2258361b2a593
Troy Straszheim <straszheim@willowgarage.com>
2011-11-12 23:17:11 -0800

    tweaks

--- toplevel.cmake
+++ toplevel.cmake
@@ -5,8 +5,6 @@ cmake_minimum_required(VERSION 2.8)
 
 set(CATKIN YES)
 list(APPEND CMAKE_PREFIX_PATH ${CMAKE_BINARY_DIR})
-# set(CATKIN_LANGS cpp)
-# set(CATKIN_LOG 9)
 
 if (IS_DIRECTORY ${CMAKE_SOURCE_DIR}/catkin)
   message("+++ catkin")
