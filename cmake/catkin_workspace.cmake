062adeaf28b3cff42ad6ec2b8a53a3907875b8b4
Troy D. Straszheim <straszheim@willowgarage.com>
2011-11-15 10:37:20 -0800

    put generated messages in single location in the toplevel

--- cmake/catkin_workspace.cmake
+++ cmake/catkin_workspace.cmake
@@ -2,6 +2,7 @@ function(catkin_workspace)
   assert(catkin_EXTRAS_DIR)
   assert_file_exists(${catkin_EXTRAS_DIR}/topologically_traverse.py.in "${catkin_EXTRAS_DIR}")
   assert_file_exists(${catkin_EXTRAS_DIR}/topologically_traverse.cmake.em "file nto found")
+  include_directories(${CMAKE_BINARY_DIR}/gen/cpp)
   em_expand(
     ${catkin_EXTRAS_DIR}/topologically_traverse.py.in
     ${CMAKE_CURRENT_BINARY_DIR}/topologically_traverse.py
