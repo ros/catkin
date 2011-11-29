f9db30b8ad5898f0bda1c2561584d3a12380dd8c
Troy D. Straszheim <straszheim@willowgarage.com>
2011-11-28 17:51:38 -0800

    thanks ethan.  closes #1.  first ticket!

--- cmake/python_version.py
+++ cmake/python_version.py
@@ -1,4 +1,5 @@
 from __future__ import print_function
 from sys import version_info as v
-print("%u.%u" % (v.major, v.minor))
+# don't use version.major, version.minor, this is pys >= 2.7
+print("%u.%u" % (v[0], v[1]))
  
