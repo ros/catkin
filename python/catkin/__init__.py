02131e09ae043292f7a768d65f8f30d6cbb5aa2e
Troy Straszheim <straszheim@willowgarage.com>
2011-11-30 16:04:14 -0800

    cmake cache reader.

--- python/catkin/__init__.py
+++ python/catkin/__init__.py
@@ -2,10 +2,19 @@ import re
 
 def load_cmake_cache(bindir):
     ifile = open(os.path.join(bindir, 'CMakeCache.txt')).readlines()
+    cache = {}
     for l in ifile:
-        if l.startswith('//') or l.startswith('\#') or len(l) < 2:
-            print "skip", l
+        if l.startswith('//') or l.startswith('#') or len(l) < 2:
             continue
+
+        m = re.match('(?P<key>[\w_-]+):(?P<type>\w+)=(?P<value>.*)$', l)
+        if m:
+            cache[m.group('key')] = m.group('value')
+        else:
+            raise RuntimeError("error parsing cmake cache line " + l)
+
+    return cache
+
 
 
 
