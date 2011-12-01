00a71eacfebc9b5352c7c104eb7a6ce2c624820f
Troy D. Straszheim <straszheim@willowgarage.com>
2011-12-01 12:03:03 -0800

    meh

---
+++ setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='catkin',
+      version= "0.0.0"
+      packages=['catkin'],
+      package_dir = {'':'python'},
+      scripts = [],
+      author = "Troy Straszheim, Morten Kjaergaard", 
+      author_email = "straszheim@willowgarage.com",
+      url = "http://www.ros.org",
+      download_url = "http://github.com/straszheim/catkin/", 
+      keywords = ["ROS"],
+      classifiers = [
+        "Programming Language :: Python", 
+        "License :: OSI Approved :: BSD License" ],
+      description = "ROS msg/srv generation", 
+      long_description = """\
+Build system stuff
+""",
+      license = "BSD"
+      )
+
