60e734cce8b22d3befd1d681af868c2dbe198be3
Troy D. Straszheim <straszheim@willowgarage.com>
2011-12-09 13:44:55 -0800

    Let's go with
    name relative/path/to/name
    
    in packages

--- cmake/update_index.py
+++ cmake/update_index.py
@@ -1,17 +1,20 @@
 #!/usr/bin/env python
 
+from __future__ import print_function
 import os, sys, yaml
 
 pkgfile = sys.argv[1]
 if os.path.exists(pkgfile):
-    y = yaml.load(open(pkgfile))
+    y = dict([l.split() for l in open(pkgfile).readlines()])
 else:
     y = {}
 
 y[sys.argv[2]] = sys.argv[3]
 
 ofile = open(pkgfile, 'w')
-yaml.dump(y, ofile, default_flow_style=False)
-ofile.close()
+for k, v in y.items():
+    print("%s %s" % (k, v), file=ofile)
+
+
 
 
