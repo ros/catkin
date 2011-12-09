d5f9c8a8e7a47aa28fba5607b11f55ee4404f466
Troy D. Straszheim <straszheim@willowgarage.com>
2011-12-09 12:18:41 -0800

    resurrect catkin's config and handle package-only packages.yaml update.

--- cmake/install_cmake_infrastructure.cmake
+++ cmake/install_cmake_infrastructure.cmake
@@ -54,8 +54,8 @@ function(install_cmake_infrastructure PACKAGE_NAME)
   file(MAKE_DIRECTORY ${CMAKE_BINARY_DIR}/etc)
   safe_execute_process(COMMAND ${catkin_EXTRAS_DIR}/update_index.py
     ${CMAKE_BINARY_DIR}/etc/packages.yaml
-    ${PACKAGE_NAME}
-    ${PACKAGE_RELATIVE_PATH}
+    "${PACKAGE_NAME}"
+    "${PACKAGE_RELATIVE_PATH}"
     )
 
   # THIS IS IMPORTANT. CMAKE_PREFIX_PATH appears to be twitchy: just
