57d1cb08a04e5e27ab85e6461a2d08222c11120d
Ethan Rublee <ethan.rublee@gmail.com>
2011-12-17 15:55:05 -0800

    Add cmake to the prefix path?

--- toplevel.cmake
+++ toplevel.cmake
@@ -4,7 +4,7 @@
 cmake_minimum_required(VERSION 2.8)
 
 set(CATKIN YES)
-list(APPEND CMAKE_PREFIX_PATH ${CMAKE_BINARY_DIR})
+list(APPEND CMAKE_PREFIX_PATH ${CMAKE_BINARY_DIR} ${CMAKE_BINARY_DIR}/cmake)
 
 if (IS_DIRECTORY ${CMAKE_SOURCE_DIR}/catkin)
   message(STATUS "+++ catkin")
