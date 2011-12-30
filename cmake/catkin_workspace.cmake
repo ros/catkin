a5c96d6add8d9d16cf6ae99a89424c129cbc7fc2
Troy Straszheim <straszheim@willowgarage.com>
2011-12-29 17:56:28 -0800

    Make sure that toplevel bin/ directory is there...  some python stuff might want to make links at cmake time.

--- cmake/catkin_workspace.cmake
+++ cmake/catkin_workspace.cmake
@@ -1,4 +1,8 @@
 function(catkin_workspace)
+  if (NOT IS_DIRECTORY ${CMAKE_BINARY_DIR}/bin)
+    file(MAKE_DIRECTORY ${CMAKE_BINARY_DIR}/bin)
+  endif()
+
   assert(catkin_EXTRAS_DIR)
   assert_file_exists(${catkin_EXTRAS_DIR}/templates/topologically_traverse.py.in "${catkin_EXTRAS_DIR}")
   assert_file_exists(${catkin_EXTRAS_DIR}/em/topologically_traverse.cmake.em "file not found")
