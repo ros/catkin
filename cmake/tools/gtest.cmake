b58b42eb3b16acbc7ef1112128dcf1865fb19cb7
Troy Straszheim <straszheim@willowgarage.com>
2012-01-10 13:32:47 -0800

    tools cleanup.

--- cmake/tools/gtest.cmake
+++ cmake/tools/gtest.cmake
@@ -26,39 +26,4 @@
 # POSSIBILITY OF SUCH DAMAGE.
 #
 
-##
-# doxygen(<TARGET_NAME> <SEARCH_DIRS>)
-# TARGET_NAME -> The cmake target to create.
-# SEARCH_DIRS -> a CMake List of directories to search for doxygenated files.
-#
-find_program(DOXYGEN_EXECUTABLE doxygen)
-
-if (DOXYGEN_EXECUTABLE)
-  set(DOXYGEN_FOUND TRUE CACHE BOOL "Doxygen found")
-endif()
-
-
-macro(catkin_doxygen TARGET_NAME SEARCH_DIRS)
-  #doxygen based docs
-  set(DOC_SEARCH_DIRS ${SEARCH_DIRS}
-  )
-  foreach(dir ${DOC_SEARCH_DIRS})
-    file(GLOB_RECURSE _doc_sources ${dir}/*)
-    list(APPEND doc_sources ${_doc_sources})
-  endforeach()
-
-  string(REPLACE ";" " " doc_sources "${doc_sources}")
-
-  configure_file(Doxyfile.in ${CMAKE_CURRENT_BINARY_DIR}/Doxyfile @ONLY)
-
-  add_custom_target(${TARGET_NAME}
-    COMMENT "Generating API documentation with Doxygen" VERBATIM
-    )
-
-  add_custom_command(TARGET ${TARGET_NAME}
-    COMMAND ${DOXYGEN_EXECUTABLE} ${CMAKE_CURRENT_BINARY_DIR}/Doxyfile
-    WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
-    )
-  add_dependencies(doc ${TARGET_NAME})
-endmacro()
-
+find_package(GTest)
