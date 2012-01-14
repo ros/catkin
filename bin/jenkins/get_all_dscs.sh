80a3a75cdedbe838eae9383e0027e84d53d3d423
Ethan <ethan.rublee@gmail.com>
2012-01-13 18:54:43 -0800

    remove some scripts from bin.

--- bin/jenkins/get_all_dscs.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash -ex
-output_dir=`pwd`/export
-mkdir -p $output_dir
-cd $output_dir
-wget -o log.txt -r http://50.28.27.175/repos/building/pool/main -nd --accept '*.dsc'
--- bin/show-upstream.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-TOP=$(cd `dirname $0` ; /bin/pwd)
-. $TOP/catkin_util.sh
-
-github_raw CONF wg-debs/$1/catkin/catkin.conf
-
-/bin/echo "conf=$CONF"
