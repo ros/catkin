5aad838e6c3cde762e40fac7596fff703ca7831d
Ethan <ethan.rublee@gmail.com>
2012-01-18 17:37:24 -0800

    make it safer

--- cmake/stack_get.py
+++ cmake/stack_get.py
@@ -5,18 +5,17 @@ import yaml, sys
 
 fields = ['Version', 'Maintainer', 'Depends']
 
-y = yaml.load(open(sys.argv[1]))
+with open(sys.argv[1]) as f:
+    y = yaml.load(f)
 
 pkg = y['Catkin-ProjectName']
 
 #need to turn the depends into semicolon seperated list
 y['Depends'] = y['Depends'].replace(',',';')
 
-ofile = open(sys.argv[2], 'w')
-
-for k, v in y.items():
-    if k in fields:
-        print('set(%s_%s "%s" CACHE INTERNAL "" FORCE)' % (pkg, k.upper(), v), file=ofile)
-
-print(r'set(CATKIN_CURRENT_STACK %s CACHE INTERNAL "" FORCE)' %pkg, file=ofile)
+with open(sys.argv[2], 'w') as ofile:
+    for k, v in y.items():
+        if k in fields:
+            print('set(%s_%s "%s" CACHE INTERNAL "" FORCE)' % (pkg, k.upper(), v), file=ofile)
+    print(r'set(CATKIN_CURRENT_STACK %s CACHE INTERNAL "" FORCE)' %pkg, file=ofile)
 
