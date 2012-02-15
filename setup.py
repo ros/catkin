9a8be9602725e9b184f22379d3b57a11858dad0a
Ken Conley <kwc@willowgarage.com>
2012-02-15 14:32:13 -0800

    catkin no longer supports sphinx within cmake.  The docs have been reorganized to follow a regular sphinx layout and use external themes/plugins as necessary

--- setup.py
+++ setup.py
@@ -2,11 +2,17 @@
 
 from distutils.core import setup
 
+import re
+search = re.search(r'Version\:\s*(\d+\.\d+\.\d+)',open('stack.yaml').read())
+if not search:
+    print >>sys.stderr, 'You must have a Version field in your stack.yaml'
+    sys.exit(-1)
+__version__ = search.groups()[0]
+
 setup(name='catkin',
-      version= "0.0.0",
-      packages=['catkin', 'catkin.sphinx'],
+      version= __version__,
+      packages=['catkin'],
       package_dir = {'catkin':'python/catkin',
-                     'catkin.sphinx': 'python/catkin/sphinx',
                      },
       scripts = [],
       author = "Troy Straszheim, Morten Kjaergaard", 
