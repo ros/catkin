b58d858fc64402dee655e940131648f7b3650de1
Dirk Thomas <dthomas@willowgarage.com>
2012-06-04 14:06:13 -0700

    removing obsolete langs.cmake, genmsg is dealing with that

--- cmake/langs.cmake
+++
@@ -1,8 +0,0 @@
-if (NOT catkin_SOURCE_DIR)
-  if (NOT CATKIN_GENLANGS)
-    file(GLOB CATKIN_GENLANGS
-      RELATIVE ${CMAKE_INSTALL_PREFIX}/etc/langs
-      ${CMAKE_INSTALL_PREFIX}/etc/langs/gen*)
-    message(STATUS "Using these generator languages from the installation: ${CATKIN_GENLANGS}")
-  endif()
-endif()
