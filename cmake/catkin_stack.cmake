56c38bf4b2883aea5249f3c45383ba565f71276a
Ioan Sucan <isucan@willowgarage.com>
2012-06-11 14:58:29 -0700

    catkin_stack() issues a warning because CATKIN_CURRENT_STACK is cached, and in between runs of cmake without clear an incorrect warning is shown. This patch uses an additional variable (not cached) to fix this issue

--- cmake/catkin_stack.cmake
+++ cmake/catkin_stack.cmake
@@ -1,19 +1,26 @@
 function(catkin_stack)
   check_unused_arguments("catkin_stack" "${ARGN}")
+
   # Related to #78.
-  if(CATKIN_CURRENT_STACK)
-    message("Warning: catkin_stack(): in ${CMAKE_CURRENT_LIST_FILE}, CATKIN_CURRENT_STACK is already set (to: ${CATKIN_CURRENT_STACK}).  This should not be the case.")
+  if(CATKIN_CURRENT_STACK_INITIALIZED)
+    message(WARNING "catkin_stack(): in ${CMAKE_CURRENT_LIST_FILE}, CATKIN_CURRENT_STACK is already set (to: ${CATKIN_CURRENT_STACK}).  This should not be the case.")
+  else()
+  
+    if(NOT CATKIN_CURRENT_STACK)
+      stamp(${CMAKE_CURRENT_SOURCE_DIR}/stack.xml)
+      
+      safe_execute_process(COMMAND ${PYTHON_EXECUTABLE}
+	${catkin_EXTRAS_DIR}/stack_get.py
+	${PROJECT_SOURCE_DIR}/stack.xml
+	${PROJECT_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/stack.cmake
+	)
+    endif()
+    include(${CMAKE_CURRENT_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/stack.cmake)
+    set(CATKIN_CURRENT_STACK_INITIALIZED True)
+
   endif()
 
-  stamp(${CMAKE_CURRENT_SOURCE_DIR}/stack.xml)
-
-  safe_execute_process(COMMAND ${PYTHON_EXECUTABLE}
-    ${catkin_EXTRAS_DIR}/stack_get.py
-    ${PROJECT_SOURCE_DIR}/stack.xml
-    ${PROJECT_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/stack.cmake
-    )
-
-  include(${CMAKE_CURRENT_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/stack.cmake)
+  message(STATUS "Building stack ${CATKIN_CURRENT_STACK}")
   install(FILES ${CMAKE_CURRENT_SOURCE_DIR}/stack.xml
           DESTINATION share/${CATKIN_CURRENT_STACK}
   )
