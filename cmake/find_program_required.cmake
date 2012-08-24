ed45e656845f4db9a05d961fd98756a292b164c7
Dirk Thomas <dthomas@willowgarage.com>
2012-08-24 10:24:03 -0700

    added missing options to find_program_required

--- cmake/find_program_required.cmake
+++ cmake/find_program_required.cmake
@@ -1,6 +1,6 @@
 function(find_program_required ARG_VAR ARG_PROGRAM_NAME)
-  parse_arguments(ARG "PATHS" "" ${ARGN})
-  find_program(${ARG_VAR} ${ARG_PROGRAM_NAME} PATHS ${ARG_PATHS})
+  parse_arguments(ARG "PATHS" "NO_DEFAULT_PATH;NO_CMAKE_FIND_ROOT_PATH" ${ARGN})
+  find_program(${ARG_VAR} ${ARG_PROGRAM_NAME} PATHS ${ARG_PATHS} ${ARG_NO_DEFAULT_PATH} ${ARG_NO_CMAKE_FIND_ROOT_PATH})
   if(NOT ${ARG_VAR})
     message(FATAL_ERROR "${ARG_PROGRAM_NAME} not found")
   else()
