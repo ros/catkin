6940f4cd53518201e56ff3593f27d2b2be27372e
Dirk Thomas <dthomas@willowgarage.com>
2012-09-03 13:22:51 -0700

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+from __future__ import print_function
 from distutils.core import setup
 import sys
 from xml.etree.ElementTree import ElementTree
@@ -7,8 +8,8 @@ from xml.etree.ElementTree import ElementTree
 try:
     root = ElementTree(None, 'stack.xml')
     version = root.findtext('version')
-except Exception, e:
-    print >> sys.stderr, 'Could not extract version from your stack.xml:\n%s' % e
+except Exception as e:
+    print('Could not extract version from your stack.xml:\n%s' % e, file=sys.stderr)
     sys.exit(-1)
 
 setup(name='catkin',
@@ -31,4 +32,3 @@ Build system stuff
 """,
       license = "BSD"
       )
-
