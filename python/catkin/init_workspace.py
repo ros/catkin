35a88daf8f8efc70aa702ac2ddbeafa9f0f37141
Dirk Thomas <dthomas@willowgarage.com>
2012-09-03 22:22:00 -0700

    make catkin scripts work without catkin being installed

--- python/catkin/init_workspace.py
+++ python/catkin/init_workspace.py
@@ -3,33 +3,48 @@ import os
 import shutil
 
 
+def _symlink_toplevel_cmake(src, dst, checked):
+    # check if toplevel file exists
+    src = os.path.join(src, 'toplevel.cmake')
+    if not os.path.exists(src):
+        checked.append(src)
+        return False
+
+    # try to symlink file
+    dst = os.path.join(dst, 'CMakeLists.txt')
+    try:
+        os.symlink(src, dst)
+        print('Creating symlink "%s" pointing to "%s"' % (dst, src))
+    except Exception:
+        # try to copy file
+        try:
+            shutil.copyfile(src, dst)
+            print('Copying file from "%s" to "%s"' % (src, dst))
+        except:
+            raise RuntimeError('Could neither symlink nor copy file "%s" to "%s"' % (src, dst))
+    return True
+
+
 def init_workspace(workspace_dir):
     env_name = 'CATKIN_WORKSPACES'
     workspaces = os.environ[env_name].split(';') if env_name in os.environ and os.environ[env_name] != '' else []
 
-    # search for file in all workspaces
+    # search for toplevel file in all workspaces
     checked = []
     for workspace in workspaces:
         parts = workspace.split(':')
         if len(parts) > 1:
-            src = os.path.join(parts[1], 'catkin', 'cmake', 'toplevel.cmake')
+            src = os.path.join(parts[1], 'catkin', 'cmake')
         else:
-            src = os.path.join(workspace, 'share', 'catkin', 'cmake', 'toplevel.cmake')
-        # skip this workspace if file does not exist
-        if not os.path.exists(src):
-            checked.append(src)
-            continue
-        # symlink/copy file
-        dst = os.path.join(workspace_dir, 'CMakeLists.txt')
-        try:
-            os.symlink(src, dst)
-            print('Creating symlink "%s" pointing to "%s"' % (dst, src))
-        except Exception:
-            try:
-                shutil.copyfile(src, dst)
-                print('Copying file from "%s" to "%s"' % (src, dst))
-            except:
-                raise RuntimeError('Could neither symlink nor copy file "%s" to "%s"' % (src, dst))
+            src = os.path.join(workspace, 'share', 'catkin', 'cmake')
+        success = _symlink_toplevel_cmake(src, workspace_dir, checked)
+        if success:
+            return
+
+    # search for toplevel file in relative location
+    src = os.path.relpath(os.path.join(os.path.dirname(__file__), '..', '..', 'cmake'))
+    success = _symlink_toplevel_cmake(src, workspace_dir, checked)
+    if success:
         return
 
     raise RuntimeError('Could not find file "catkin/cmake/toplevel.cmake" in any workspace, checked the following paths:\n%s' % ('\n'.join(checked)))
