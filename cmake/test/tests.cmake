f931db5c8c14475a9d74ffc65b9dbbe45c98d11d
Dirk Thomas <dthomas@willowgarage.com>
2012-09-11 01:22:28 -0700

    added hidden run_tests hierarchy which enables the run_tests target to clean the test result before running all tests without performing any cleaning for project-specific run_tests_* targets

--- cmake/test/tests.cmake
+++ cmake/test/tests.cmake
@@ -19,14 +19,9 @@ if(NOT TARGET run_tests)
 endif()
 
 # create target to clean test results
-if(NOT TARGET clean-test-results)
-  # clean out previous test results before running tests
-  add_custom_target(clean-test-results
+if(NOT TARGET clean_test_results)
+  add_custom_target(clean_test_results
     COMMAND ${CMAKE_COMMAND} -E remove_directory ${CATKIN_TEST_RESULTS_DIR})
-  # make the 'tests' target depend on 'clean-test-results'
-  # which will ensure that test results are deleted before we try to build all tests
-  # and thus before we try to run all tests
-  add_dependencies(tests clean-test-results)
 endif()
 
 #
@@ -47,12 +42,18 @@ function(catkin_run_tests_target type name xunit_filename)
   # create meta target to trigger all tests of a project
   if(NOT TARGET run_tests_${PROJECT_NAME})
     add_custom_target(run_tests_${PROJECT_NAME})
-    add_dependencies(run_tests run_tests_${PROJECT_NAME})
+    # create hidden meta target which depends on hidden test targets which depend on clean_test_results
+    add_custom_target(.run_tests_${PROJECT_NAME})
+    # run_tests depends on this hidden target hierarchy to clear test results before running all tests
+    add_dependencies(run_tests .run_tests_${PROJECT_NAME})
   endif()
   # create meta target to trigger all tests of a specific type of a project
   if(NOT TARGET run_tests_${PROJECT_NAME}_${type})
     add_custom_target(run_tests_${PROJECT_NAME}_${type})
     add_dependencies(run_tests_${PROJECT_NAME} run_tests_${PROJECT_NAME}_${type})
+    # hidden meta target which depends on hidden test targets which depend on clean_test_results
+    add_custom_target(.run_tests_${PROJECT_NAME}_${type})
+    add_dependencies(.run_tests_${PROJECT_NAME} .run_tests_${PROJECT_NAME}_${type})
   endif()
   # create target for test execution
   set(results ${CATKIN_TEST_RESULTS_DIR}/${PROJECT_NAME}/${xunit_filename})
@@ -60,8 +61,14 @@ function(catkin_run_tests_target type name xunit_filename)
     set(working_dir_arg "--working-dir" ${_testing_WORKING_DIRECTORY})
   endif()
   assert(CATKIN_ENV)
-  add_custom_target(run_tests_${PROJECT_NAME}_${type}_${name}
-    COMMAND ${CATKIN_ENV} ${PYTHON_EXECUTABLE}
+  set(cmd ${CATKIN_ENV} ${PYTHON_EXECUTABLE}
     ${catkin_EXTRAS_DIR}/test/run_tests.py --results ${results} ${working_dir_arg} ${_testing_COMMAND})
+  add_custom_target(run_tests_${PROJECT_NAME}_${type}_${name}
+    COMMAND ${cmd})
   add_dependencies(run_tests_${PROJECT_NAME}_${type} run_tests_${PROJECT_NAME}_${type}_${name})
+  # hidden test target which depends on clean_test_results
+  add_custom_target(.run_tests_${PROJECT_NAME}_${type}_${name}
+    COMMAND ${cmd})
+  add_dependencies(.run_tests_${PROJECT_NAME}_${type} .run_tests_${PROJECT_NAME}_${type}_${name})
+  add_dependencies(.run_tests_${PROJECT_NAME}_${type}_${name} clean_test_results)
 endfunction()
