4cc18d54e5722e7ca985b5b1abaaea36c6165c53
Dirk Thomas <dthomas@willowgarage.com>
2012-09-14 10:56:45 -0700

    added comment to list_append_unique to justify its use over vanilla cmake

--- cmake/list_append_unique.cmake
+++ cmake/list_append_unique.cmake
@@ -1,8 +1,15 @@
+#
+# Append elements to a list if they are not already in the list.
+#
+# .. note:: Using CMake's ``list(APPEND ..)`` and
+#   ``list(REMOVE_DUPLICATES ..)`` since its implementation uses a
+#   set internally which make the operation unstable.
+#
 macro(list_append_unique listname)
-  foreach(item ${ARGN})
-    list(FIND ${listname} ${item} _index)
+  foreach(_item ${ARGN})
+    list(FIND ${listname} ${_item} _index)
     if(_index EQUAL -1)
-      list(APPEND ${listname} ${item})
+      list(APPEND ${listname} ${_item})
     endif()
   endforeach()
 endmacro()
