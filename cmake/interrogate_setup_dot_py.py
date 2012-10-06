90f5523f40b0a0158e23573848227f2000913c23
Thibault Kruse <kruset@in.tum.de>
2012-10-06 15:30:18 +0200

    moved mysetup function into Dummy class

--- cmake/interrogate_setup_dot_py.py
+++ cmake/interrogate_setup_dot_py.py
@@ -26,68 +26,71 @@ def samefile(path1, path2):
                  os.path.normcase(os.path.normpath(path2))
     return result
 
-
-def mysetup(*args, **kwargs):
-    if 'version' not in kwargs:
-        print("""
-    *** Unable to find 'version' in setup.py of %s
-""" % PACKAGE_NAME)
-        raise RuntimeError("version not found in setup.py")
-
-    with open(OUTFILE, 'w') as out:
-
-        print(r'set(%s_VERSION "%s")' % (PACKAGE_NAME, kwargs['version']),
-              file=out)
-        print(r'set(%s_SCRIPTS "%s")' % (PACKAGE_NAME,
-                                         ';'.join(kwargs.get('scripts', []))), file=out)
-
-        if 'package_dir' not in kwargs:
-            raise RuntimeError(r'package_dir not in setup.py', file=sys.stderr)
-
-        package_dir = kwargs['package_dir']
-        allprefix = package_dir.get('', None)
-
-        pkgs = kwargs.get('packages', [])
-
-        # Remove packages with '.' separators.  setuptools requires
-        # specifying submodules.  The symlink approach of catkin does not
-        # work with submodules.  In the common case, this does not matter
-        # as the submodule is within the containing module.  We verify
-        # this assumption, and if it passes, we remove submodule packages.
-        if not allprefix:
-            for pkg in pkgs:
-                splits = pkg.split('.')
-                # hack: ignore write-combining setup.py files for msg and srv
-                # files
-                if len(splits) > 1 and splits[1] not in ['msg', 'srv']:
-                    top_level = splits[0]
-                    top_level_dir = package_dir[top_level]
-                    expected_dir = os.path.join(top_level_dir, os.path.join(*splits[1:]))
-                    actual_dir = package_dir[pkg]
-                    if not samefile(expected_dir, actual_dir):
-                        raise RuntimeError("catkin_export_python does not support setup.py files that combine across multiple directories")
-
-        # If checks pass, remove all submodules
-        pkgs = [p for p in pkgs if '.' not in p]
-
-        resolved_pkgs = []
-        for pkg in pkgs:
-            if allprefix:
-                resolved_pkgs += [os.path.join(allprefix, pkg)]
-            else:
-                resolved_pkgs += [package_dir[pkg]]
-
-        print(r'set(%s_PACKAGES "%s")' % (PACKAGE_NAME, ';'.join(pkgs)), file=out)
-        print(r'set(%s_PACKAGE_DIRS "%s")' % (PACKAGE_NAME, ';'.join(resolved_pkgs).replace("\\", "/")), file=out)
-
-
 class Dummy:
-    pass
+    '''
+    Used as a replacement for modules setuptools and distutils.core,
+    defines a setup function.
+    '''
+
+    def setup(*args, **kwargs):
+        if 'version' not in kwargs:
+            print("""
+        *** Unable to find 'version' in setup.py of %s
+""" % PACKAGE_NAME)
+            raise RuntimeError("version not found in setup.py")
+
+        with open(OUTFILE, 'w') as out:
+
+            print(r'set(%s_VERSION "%s")' % (PACKAGE_NAME, kwargs['version']),
+                  file=out)
+            print(r'set(%s_SCRIPTS "%s")' % (PACKAGE_NAME,
+                                             ';'.join(kwargs.get('scripts', []))), file=out)
+
+            if 'package_dir' not in kwargs:
+                raise RuntimeError(r'package_dir not in setup.py', file=sys.stderr)
+
+            package_dir = kwargs['package_dir']
+            allprefix = package_dir.get('', None)
+
+            pkgs = kwargs.get('packages', [])
+
+            # Remove packages with '.' separators.  setuptools requires
+            # specifying submodules.  The symlink approach of catkin does not
+            # work with submodules.  In the common case, this does not matter
+            # as the submodule is within the containing module.  We verify
+            # this assumption, and if it passes, we remove submodule packages.
+            if not allprefix:
+                for pkg in pkgs:
+                    splits = pkg.split('.')
+                    # hack: ignore write-combining setup.py files for msg and srv
+                    # files
+                    if len(splits) > 1 and splits[1] not in ['msg', 'srv']:
+                        top_level = splits[0]
+                        top_level_dir = package_dir[top_level]
+                        expected_dir = os.path.join(top_level_dir,
+                                                    os.path.join(*splits[1:]))
+                        actual_dir = package_dir[pkg]
+                        if not samefile(expected_dir, actual_dir):
+                            raise RuntimeError(
+                                "catkin_export_python does not support setup.py files that combine across multiple directories")
+
+            # If checks pass, remove all submodules
+            pkgs = [p for p in pkgs if '.' not in p]
+
+            resolved_pkgs = []
+            for pkg in pkgs:
+                if allprefix:
+                    resolved_pkgs += [os.path.join(allprefix, pkg)]
+                else:
+                    resolved_pkgs += [package_dir[pkg]]
+
+            print(r'set(%s_PACKAGES "%s")' % (PACKAGE_NAME, ';'.join(pkgs)), file=out)
+            print(r'set(%s_PACKAGE_DIRS "%s")' % (PACKAGE_NAME, ';'.join(resolved_pkgs).replace("\\", "/")), file=out)
+
 
 
 def main():
     dummy = Dummy()
-    setattr(dummy, 'setup', mysetup)
 
     sys.modules['setuptools'] = dummy
     sys.modules['distutils.core'] = dummy
