511136b51a1dadac218eeef2b9cad60d34b941aa
Thibault Kruse <kruset@in.tum.de>
2012-10-08 16:34:50 +0200

    docstring

--- python/catkin/find_in_workspaces.py
+++ python/catkin/find_in_workspaces.py
@@ -60,11 +60,13 @@ def find_in_workspaces(search_dirs=None, project=None, path=None, _workspaces=ge
     All workspaces are searched in order.
     Each workspace, each search_in subfolder, the project name and the path are concatenated to define a candidate path.
     If the candidate path exists it is appended to the result list.
+
     :param search_dir: The list of subfolders to search in (default contains all valid values: 'bin', 'etc', 'lib', 'libexec', 'share'), ``list``
-    :param project: The project name to search for (not possible with the global search_in folders 'bin' and 'lib'), ``str``
+    :param project: The project name to search for (optional, not possible with the global search_in folders 'bin' and 'lib'), ``str``
     :param path: The path, ``str``
     Note: the search might return multiple paths for a 'share' from build- and source-space.
     :param _workspaces: (optional, used for unit tests), the list of workspaces to use.
+    :raises ValueError: if search_dirs contains an invalid folder name
     :returns: List of paths, ``list``
     '''
     search_dirs = _get_valid_search_dirs(search_dirs, project)
