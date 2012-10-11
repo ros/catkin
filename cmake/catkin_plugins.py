246eaa3cf83335d16637af39714c3dc0f5397534
Thibault Kruse <kruset@in.tum.de>
2012-10-11 14:27:56 +0200

    refactor argparse and get_output * argparse mutual required using argparse group * _get_output returns list for easy unit test unit test

--- cmake/catkin_plugins.py
+++ cmake/catkin_plugins.py
@@ -1,8 +1,5 @@
 #!/usr/bin/env python
 
-
-
-
 from __future__ import print_function
 import argparse
 import sys
@@ -13,36 +10,37 @@ from catkin_pkg.package import parse_package
 
 def parse_args(args=sys.argv[1:]):
     parser = argparse.ArgumentParser(description='Provides the plugin xml exports for the package.xml found in the path to be inserted into the generated manifest.xml.')
-    parser.add_argument('file', help='The project to find plugins for')
-    parser.add_argument('--exports', dest="exports", action="store_true", help='Generate the exports')
-    parser.add_argument('--depends', dest="depends", action="store_true", help='Generate the necessary depends')
+    parser.add_argument('package', help='The package to find plugins for')
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument('--exports', dest="exports", action="store_true", help='Generate the exports')
+    group.add_argument('--depends', dest="depends", action="store_true", help='Generate the necessary depends')
     args = parser.parse_args(args=args)
-    if not (args.exports or args.depends ):
-        parser.error("exports or depends options required")
     return args
 
 
+def _get_output(package, exports=True, depends=False):
+    output = []
+    plugins = []
+    for export in package.exports:
+        if 'plugin' in export.attributes:
+            plugins.append(export)
 
+    for plugin in plugins:
+        if exports:
+            output.append('<%s plugin="%s"/>' % (plugin.tagname, plugin.attributes['plugin']))
+        elif depends:
+            if plugin.tagname != package.name:
+                output.append('<depend package="%s"/>' % plugin.tagname)
+    return output
 
 
 def main():
     args = parse_args()
 
-    plugins = []
     try:
-        package = parse_package(args.file)
-        for e in package.exports:
-            if 'plugin' in e.attributes:
-                plugins.append(e)
-
-        for p in plugins:
-            if args.exports:
-                print('<%s plugin="%s"/>' % (p.tagname, p.attributes['plugin']) )
-            elif args.depends:
-                if p.tagname != package.name:
-                    print('<depend package="%s"/>' % p.tagname)
-
-
+        package = parse_package(args.package)
+        output = _get_output(package, args.exports, args.depends)
+        print('\n'.join(output))
     except Exception as e:
         sys.exit(str(e))
 
