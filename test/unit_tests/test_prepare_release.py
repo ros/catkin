a22d995ef54f4f920a86b597a19749cd7c329a6f
Thibault Kruse <kruset@in.tum.de>
2012-10-12 11:28:33 +0200

    make tests run from buildfolder

--- test/unit_tests/test_prepare_release.py
+++ test/unit_tests/test_prepare_release.py
@@ -5,7 +5,6 @@ import imp
 imp.load_source('catkin_prepare_release',
                 os.path.join(os.path.dirname(__file__),
                              '..', '..', 'bin', 'catkin_prepare_release'))
-
 from catkin_prepare_release import get_commands
 
 
@@ -13,38 +12,36 @@ class CatkinPrepareReleaseTest(unittest.TestCase):
 
     def test_get_commands(self):
         commands = get_commands('foo', {}, 'git', '0.1.2', svn_url=None)
-        self.assertEqual(['bin/catkin_package_version --bump patch foo',
-                          'git commit -m "0.1.2" ',
+        # command path varies depending on where tests are run
+        self.assertTrue(commands[0].endswith('catkin_package_version --bump patch foo'))
+        self.assertEqual(['git commit -m "0.1.2" ',
                           'git push',
                           'git tag 0.1.2',
-                          'git push --tags'], commands)
+                          'git push --tags'], commands[1:])
 
         commands = get_commands('foo', {'pack1': 'bar'}, 'git', '0.1.2', svn_url=None)
-        self.assertEqual(['bin/catkin_package_version --bump patch foo',
-                          'git commit -m "0.1.2" pack1/package.xml',
+        self.assertEqual(['git commit -m "0.1.2" pack1/package.xml',
                           'git push',
                           'git tag 0.1.2',
-                          'git push --tags'], commands)
+                          'git push --tags'], commands[1:])
 
         commands = get_commands('foo', {'pack1': 'bar', 'pack2': 'foo'}, 'git', '0.1.2', svn_url=None)
-        self.assertEqual(['bin/catkin_package_version --bump patch foo',
-                          'git commit -m "0.1.2" pack1/package.xml pack2/package.xml',
+        self.assertEqual(['git commit -m "0.1.2" pack1/package.xml pack2/package.xml',
                           'git push',
                           'git tag 0.1.2',
-                          'git push --tags'], commands)
+                          'git push --tags'], commands[1:])
 
     def test_get_commands_svn(self):
+        # command path varies depending on where tests are run
         commands = get_commands('foo', {}, 'svn', '0.1.2', svn_url='http://foo.bar/project1/trunk')
-        self.assertEqual(['bin/catkin_package_version --bump patch foo',
-                          'svn commit -m "0.1.2" ',
-                          'svn cp -m "tagging 0.1.2" http://foo.bar/project1/trunk http://foo.bar/project1/tags/0.1.2'], commands)
+        self.assertTrue(commands[0].endswith('catkin_package_version --bump patch foo'))
+        self.assertEqual(['svn commit -m "0.1.2" ',
+                          'svn cp -m "tagging 0.1.2" http://foo.bar/project1/trunk http://foo.bar/project1/tags/0.1.2'], commands[1:])
 
         commands = get_commands('foo', {}, 'svn', '0.1.2', svn_url='http://foo.bar/project1/branches/branch_foo')
-        self.assertEqual(['bin/catkin_package_version --bump patch foo',
-                          'svn commit -m "0.1.2" ',
-                          'svn cp -m "tagging 0.1.2" http://foo.bar/project1/branches/branch_foo http://foo.bar/project1/tags/0.1.2'], commands)
+        self.assertEqual(['svn commit -m "0.1.2" ',
+                          'svn cp -m "tagging 0.1.2" http://foo.bar/project1/branches/branch_foo http://foo.bar/project1/tags/0.1.2'], commands[1:])
 
         commands = get_commands('foo', {}, 'svn', '0.1.2', svn_url='http://foo.bar/project1/tags/tag_foo')
-        self.assertEqual(['bin/catkin_package_version --bump patch foo',
-                          'svn commit -m "0.1.2" ',
-                          'svn cp -m "tagging 0.1.2" http://foo.bar/project1/tags/tag_foo http://foo.bar/project1/tags/0.1.2'], commands)
+        self.assertEqual(['svn commit -m "0.1.2" ',
+                          'svn cp -m "tagging 0.1.2" http://foo.bar/project1/tags/tag_foo http://foo.bar/project1/tags/0.1.2'], commands[1:])
