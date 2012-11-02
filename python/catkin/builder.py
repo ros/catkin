943f80380787fdc289af348da819a5d83553e80f
Dirk Thomas <dthomas@willowgarage.com>
2012-11-02 11:32:23 -0700

    added install option for build isolated

--- python/catkin/builder.py
+++ python/catkin/builder.py
@@ -5,7 +5,7 @@ import subprocess
 from catkin_pkg.topological_order import topological_order
 
 
-def build_workspace_in_isolation(sourcespace_dir, buildspace_parent_dir):
+def build_workspace_in_isolation(sourcespace_dir, buildspace_parent_dir, install=False):
     sourcespace_dir = os.path.abspath(sourcespace_dir)
     packages = topological_order(sourcespace_dir)
 
@@ -13,7 +13,7 @@ def build_workspace_in_isolation(sourcespace_dir, buildspace_parent_dir):
     if not os.path.exists(buildspace_dir):
         os.makedirs(buildspace_dir)
 
-    last_package_build_dir = None
+    last_env_to_source = None
     for path, package in packages:
         if 'metapackage' in [e.tagname for e in package.exports]:
             print('\n+++ Skipping metapackage "%s"' % path)
@@ -22,22 +22,31 @@ def build_workspace_in_isolation(sourcespace_dir, buildspace_parent_dir):
         package_build_dir = os.path.join(buildspace_dir, package.name)
         if not os.path.exists(package_build_dir):
             os.mkdir(package_build_dir)
+        if install:
+            package_install_dir = os.path.join(package_build_dir, '__install')
+            if not os.path.exists(package_install_dir):
+                os.mkdir(package_install_dir)
 
         makefile = os.path.join(package_build_dir, 'Makefile')
         if not os.path.exists(makefile):
             cmd = ['cmake', os.path.dirname(package.filename), '-DCATKIN_STATIC_ENV=1']
-            _run_command_with_env(cmd, package_build_dir, last_package_build_dir)
+            if install:
+                cmd.append('-DCMAKE_INSTALL_PREFIX=%s' % package_install_dir)
+            _run_command_with_env(cmd, package_build_dir, last_env_to_source)
 
         cmd = ['make', '-j8']
-        _run_command_with_env(cmd, package_build_dir, last_package_build_dir)
+        _run_command_with_env(cmd, package_build_dir, last_env_to_source)
 
-        last_package_build_dir = package_build_dir
+        if not install:
+            last_env_to_source = os.path.join(package_build_dir, 'catkin_generated', 'env_cached.sh')
+        else:
+            cmd = ['make', 'install']
+            _run_command_with_env(cmd, package_build_dir, last_env_to_source)
+            last_env_to_source = os.path.join(package_install_dir, 'env.sh')
 
 
-def _run_command_with_env(cmd, cwd, last_package_build_dir=None):
-    if last_package_build_dir is not None:
-        #last_env_sh = os.path.join(last_package_build_dir, 'buildspace', 'env.sh')
-        last_env_sh = os.path.join(last_package_build_dir, 'catkin_generated', 'env_cached.sh')
-        cmd = [last_env_sh] + cmd
+def _run_command_with_env(cmd, cwd, env_to_source=None):
+    if env_to_source is not None:
+        cmd = [env_to_source] + cmd
     print('\n    Running command: %s in %s' % (cmd, cwd))
     subprocess.check_call(cmd, cwd=cwd)
