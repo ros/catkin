7cb3c14ae4815021e32780db82d6bed1b7547fb4
Thibault Kruse <kruset@in.tum.de>
2012-11-05 10:52:13 +0100

    docstring and unit test

---
+++ test/unit_tests/test_builder.py
@@ -0,0 +1,58 @@
+import os
+import unittest
+import tempfile
+import shutil
+import mock
+
+try:
+    import catkin.builder
+    from catkin.builder import build_workspace_in_isolation
+except ImportError as impe:
+    raise ImportError(
+        'Please adjust your pythonpath before running this test: %s' % str(impe))
+
+
+class BuilderTest(unittest.TestCase):
+
+    def test_build_workspace_in_isolation(self):
+        backup_top = catkin.builder.topological_order
+        backup_run = catkin.builder._run_command_with_env
+        try:
+            root_dir = tempfile.mkdtemp()
+            mockp1 = mock.Mock()
+            mockp1.exports = []
+            mockp1.name = 'p1'
+            mockp1.filename = '/foo/package.xml'
+            mockp2 = mock.Mock()
+            mockp2.exports = []
+            mockp2.name = 'p2'
+            mockp2.filename = '/bar/package.xml'
+            mock_top = mock.Mock()
+            mock_top.return_value = [('foopack', mockp1), ('barpack', mockp2)]
+            mock_run = mock.Mock()
+            catkin.builder.topological_order = mock_top
+            catkin.builder._run_command_with_env = mock_run
+            build_workspace_in_isolation('/path1', root_dir)
+            self.assertEqual(1, mock_top.call_count)
+            top_args = mock_top.call_args[0]
+            self.assertEqual(('/path1',), top_args)
+            self.assertEqual(4, mock_run.call_count, mock_run.call_args_list)
+            run_args = mock_run.call_args_list
+            self.assertEqual(['cmake', '/foo', '-DCATKIN_STATIC_ENV=1'], run_args[0][0][0])
+            self.assertEqual(['make', '-j8'], run_args[1][0][0])
+            # install case
+            mock_top.reset_mock()
+            mock_run.reset_mock()
+            build_workspace_in_isolation('/path1', root_dir, install=True)
+            self.assertEqual(1, mock_top.call_count)
+            top_args = mock_top.call_args[0]
+            self.assertEqual(('/path1',), top_args)
+            self.assertEqual(6, mock_run.call_count)
+            run_args = mock_run.call_args_list
+            self.assertEqual(['cmake', '/foo', '-DCATKIN_STATIC_ENV=1', '-DCMAKE_INSTALL_PREFIX=%s' % os.path.join(root_dir, 'build_isolated/p1__install')], run_args[0][0][0])
+            self.assertEqual(['make', '-j8'], run_args[1][0][0])
+            self.assertEqual(['make', 'install'], run_args[2][0][0])
+        finally:
+            catkin.builder.topological_order = backup_top
+            catkin.builder._run_command_with_env = backup_run
+            shutil.rmtree(root_dir)
