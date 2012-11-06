f6c8a98b00b1aade681d7b01a49b14001bb04000
Dirk Thomas <dthomas@willowgarage.com>
2012-11-05 17:53:02 -0800

    updated builder unit test

--- test/unit_tests/test_builder.py
+++ test/unit_tests/test_builder.py
@@ -17,8 +17,10 @@ class BuilderTest(unittest.TestCase):
     def test_build_workspace_in_isolation(self):
         backup_top = catkin.builder.topological_order
         backup_run = catkin.builder._run_command_with_env
+        old_cwd = os.getcwd()
         try:
             root_dir = tempfile.mkdtemp()
+            os.chdir(root_dir)
             mockp1 = mock.Mock()
             mockp1.exports = []
             mockp1.name = 'p1'
@@ -32,27 +34,28 @@ class BuilderTest(unittest.TestCase):
             mock_run = mock.Mock()
             catkin.builder.topological_order = mock_top
             catkin.builder._run_command_with_env = mock_run
-            build_workspace_in_isolation('/path1', root_dir)
+            build_workspace_in_isolation('/path1')
             self.assertEqual(1, mock_top.call_count)
             top_args = mock_top.call_args[0]
             self.assertEqual(('/path1',), top_args)
             self.assertEqual(4, mock_run.call_count, mock_run.call_args_list)
             run_args = mock_run.call_args_list
-            self.assertEqual(['cmake', '/foo', '-DCATKIN_STATIC_ENV=1'], run_args[0][0][0])
+            self.assertEqual(['cmake', '/foo', '-DCATKIN_STATIC_ENV=1', '-DCATKIN_DEVEL_PREFIX=%s' % os.path.join(root_dir, 'p1__devel')], run_args[0][0][0])
             self.assertEqual(['make', '-j8'], run_args[1][0][0])
             # install case
             mock_top.reset_mock()
             mock_run.reset_mock()
-            build_workspace_in_isolation('/path1', root_dir, install=True)
+            build_workspace_in_isolation('/path1', install=True)
             self.assertEqual(1, mock_top.call_count)
             top_args = mock_top.call_args[0]
             self.assertEqual(('/path1',), top_args)
             self.assertEqual(6, mock_run.call_count)
             run_args = mock_run.call_args_list
-            self.assertEqual(['cmake', '/foo', '-DCATKIN_STATIC_ENV=1', '-DCMAKE_INSTALL_PREFIX=%s' % os.path.join(root_dir, 'build_isolated/p1__install')], run_args[0][0][0])
+            self.assertEqual(['cmake', '/foo', '-DCATKIN_STATIC_ENV=1', '-DCATKIN_DEVEL_PREFIX=%s' % os.path.join(root_dir, 'p1__devel'), '-DCMAKE_INSTALL_PREFIX=%s' % os.path.join(root_dir, 'p1__install')], run_args[0][0][0])
             self.assertEqual(['make', '-j8'], run_args[1][0][0])
             self.assertEqual(['make', 'install'], run_args[2][0][0])
         finally:
+            os.chdir(old_cwd)
             catkin.builder.topological_order = backup_top
             catkin.builder._run_command_with_env = backup_run
             shutil.rmtree(root_dir)
