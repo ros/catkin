4454f9183512d94cd1f780b80a4ee734d3954079
Dirk Thomas <dthomas@willowgarage.com>
2012-12-05 18:58:01 -0800

    moved order_paths to catkin_pkg

--- cmake/order_paths.py
+++ cmake/order_paths.py
@@ -4,6 +4,12 @@ from __future__ import print_function
 
 from argparse import ArgumentParser
 import os
+import sys
+
+try:
+    from catkin_pkg.workspaces import order_paths
+except ImportError as e:
+    sys.exit('ImportError: "from catkin_pkg.package import parse_package" failed: %s\nMake sure that you have installed "catkin_pkg", it is up to date and on the PYTHONPATH.' % e)
 
 
 def main():
@@ -25,28 +31,6 @@ def main():
 
     with open(args.outfile, 'w') as fh:
         fh.write('set(ORDERED_PATHS "%s")' % ';'.join(ordered_paths))
-
-
-def order_paths(paths_to_order, prefix_paths):
-    """
-    returns a list containing all items of path_to_order ordered by list of prefix_paths, compared as strings
-    :param paths_to_order: list of paths
-    :param prefix_paths: list of paths, must not end with '/'
-    """
-    # the ordered paths contains a list for each prefix plus one more which contains paths which do not match one of the prefix_paths
-    ordered_paths = [[] for _ in range(len(prefix_paths) + 1)]
-
-    for path in paths_to_order:
-        # put each include directory into the slot where it matches the prefix, or last otherwise
-        index = 0
-        for prefix in prefix_paths:
-            if path == prefix or path.startswith(prefix + os.sep) or (os.altsep and path.startswith(prefix + os.altsep)):
-                break
-            index += 1
-        ordered_paths[index].append(path)
-
-    # flatten list of lists
-    return [j for i in ordered_paths for j in i]
 
 
 if __name__ == '__main__':
