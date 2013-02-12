fbb4c326c606c1f671f46ca359b50ace224cfa08
Dirk Thomas <dthomas@willowgarage.com>
2013-02-11 17:42:52 -0800

    fixed debug message

--- cmake/safe_execute_process.cmake
+++ cmake/safe_execute_process.cmake
@@ -4,7 +4,7 @@ macro(safe_execute_process cmd_keyword arg1)
     set(_cmd "${_cmd} ${_arg}")
   endforeach()
 
-  debug_message(2 "execute_process(${cmd})")
+  debug_message(2 "execute_process(${_cmd})")
   execute_process(${ARGV} RESULT_VARIABLE _res)
 
   if(NOT _res EQUAL 0)
