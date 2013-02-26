ros/catkin

commit cb5564734d5b4ae3284b109b286fc4c543bb5493

modified handling of metapackages, they must have a CMakeLists.txt for catkin_make_isolated, catkin_make will generate one on demand and warn about it (#349)

cmake/catkin_workspace.cmake
@@ -35,6 +35,7 @@ function(catkin_workspace)
   include(${CMAKE_CURRENT_BINARY_DIR}/catkin_generated/order_packages.cmake)
 
   if(CATKIN_ORDERED_PACKAGES)
+    set(CATKIN_NONCONFORMANT_METAPACKAGE FALSE)
     set(CATKIN_NONHOMOGENEOUS_WORKSPACE FALSE)
     message(STATUS "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
     message(STATUS "~~  traversing packages in topological order:")
@@ -42,10 +43,26 @@ function(catkin_workspace)
     math(EXPR range "${count} - 1")
     foreach(index RANGE ${range})
       list(GET CATKIN_ORDERED_PACKAGES ${index} name)
+      list(GET CATKIN_ORDERED_PACKAGE_PATHS ${index} path)
       list(GET CATKIN_ORDERED_PACKAGES_IS_META ${index} is_meta)
       list(GET CATKIN_ORDERED_PACKAGES_BUILD_TYPE ${index} build_type)
       if(${is_meta})
         message(STATUS "~~  - ${name} (metapackage)")
+        # verify that CMakeLists.txt of metapackage conforms to standard
+        configure_file(${catkin_EXTRAS_DIR}/templates/metapackage.cmake.in
+          ${CMAKE_CURRENT_BINARY_DIR}/catkin_generated/metapackages/${name}/CMakeLists.txt
+          @ONLY)
+        if(EXISTS ${path}/CMakeLists.txt)
+          # compare CMakeLists.txt with standard content
+          file(STRINGS ${CMAKE_CURRENT_BINARY_DIR}/catkin_generated/metapackages/${name}/CMakeLists.txt generated_cmakelists)
+          file(STRINGS ${path}/CMakeLists.txt existing_cmakelists)
+          if(NOT ${generated_cmakelists} STREQUAL ${existing_cmakelists})
+            set(CATKIN_NONHOMOGENEOUS_WORKSPACE TRUE)
+            message("WARNING: The CMakeLists.txt of the metapackage '${name}' contains non standard content. Use the content of the following file instead: ${CMAKE_CURRENT_BINARY_DIR}/catkin_generated/metapackages/${name}/CMakeLists.txt")
+          endif()
+        else()
+          message("WARNING: The metapackage '${name}' has no CMakeLists.txt. Please add one to the package source. You can use the following file: ${CMAKE_CURRENT_BINARY_DIR}/catkin_generated/metapackages/${name}/CMakeLists.txt")
+        endif()
       else()
         if(${build_type} MATCHES catkin)
           message(STATUS "~~  - ${name}")
@@ -62,6 +79,9 @@ function(catkin_workspace)
     endforeach()
     message(STATUS "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
 
+    if(${CATKIN_NONCONFORMANT_METAPACKAGE})
+      message(FATAL_ERROR "This workspace contains metapackages with a non-standard CMakeLists.txt.")
+    endif()
     if(${CATKIN_NONHOMOGENEOUS_WORKSPACE})
       message(FATAL_ERROR "This workspace contains non-catkin packages in it, and catkin cannot build a non-homogeneous workspace without isolation.")
     endif()
@@ -72,15 +92,20 @@ function(catkin_workspace)
       list(GET CATKIN_ORDERED_PACKAGES_IS_META ${index} is_meta)
       list(GET CATKIN_ORDERED_PACKAGES_BUILD_TYPE ${index} build_type)
       if(${is_meta})
-        message(STATUS "+++ skipping metapackage: '${name}'")
-      else()
-        if(${build_type} MATCHES catkin)
-          message(STATUS "+++ processing catkin package: '${name}'")
+        message(STATUS "+++ processing catkin metapackage: '${name}'")
+        if(EXISTS ${path}/CMakeLists.txt)
           message(STATUS "==> add_subdirectory(${path})")
           add_subdirectory(${path})
         else()
-          message(FATAL_ERROR "Non-catkin package found, non-homogeneous workspaces are not supported.")
+          message(STATUS "==> add_subdirectory(${path}) (using generated file from <buildspace>/catkin_generated/metapackages/${name})")
+          add_subdirectory(${CMAKE_CURRENT_BINARY_DIR}/catkin_generated/metapackages/${name} ${CMAKE_BINARY_DIR}/${path})
         endif()
+      elseif(${build_type} MATCHES catkin)
+        message(STATUS "+++ processing catkin package: '${name}'")
+        message(STATUS "==> add_subdirectory(${path})")
+        add_subdirectory(${path})
+      else()
+        message(FATAL_ERROR "Non-catkin package found, non-homogeneous workspaces are not supported.")
       endif()
     endforeach()
   endif()

bin/catkin_make
@@ -85,13 +85,11 @@ def main():
 
     packages = find_packages(source_path, exclude_subspaces=True)
 
-    # verify that specified package exists in workspace and is not a metapackage
+    # verify that specified package exists in workspace
     if args.pkg:
         packages_by_name = {p.name: path for path, p in packages.iteritems()}
         if args.pkg not in packages_by_name:
             return fmt('@{rf}Package @{boldon}"%s"@{boldoff} not found in the workspace' % args.pkg)
-        if 'metapackage' in [e.tagname for e in packages[packages_by_name[args.pkg]].exports]:
-            return fmt('@{rf}Package @{boldon}"%s"@{boldoff} is a metapackage and can not be built' % args.pkg)
 
     # check if cmake must be run (either for a changed list of package paths or changed cmake arguments)
     force_cmake = cmake_input_changed(packages, build_path, cmake_args)

cmake/all.cmake
@@ -102,6 +102,7 @@ foreach(filename
     catkin_add_env_hooks
     catkin_destinations
     catkin_generate_environment
+    catkin_metapackage
     catkin_package
     catkin_package_xml
     catkin_workspace

cmake/catkin_metapackage.cmake
@@ -1,0 +1,44 @@
+#
+# It installs the package.xml file of a metapackage.
+#
+# .. note:: It must be called once for each metapackage.  Best
+#   practice is to call this macro early in your root CMakeLists.txt,
+#   immediately after calling ``project()`` and
+#   ``find_package(catkin REQUIRED)``.
+#
+# @public
+#
+function(catkin_metapackage)
+  if(ARGN)
+    message(FATAL_ERROR "catkin_metapackage() called with unused arguments: ${ARGN}")
+  endif()
+
+  debug_message(10 "catkin_metapackage() called in file ${CMAKE_CURRENT_LIST_FILE}")
+
+  # verify that project() has been called before
+  if(NOT PROJECT_NAME)
+    message(FATAL_ERROR "catkin_metapackage() PROJECT_NAME is not set. You must call project() before calling catkin_metapackage().")
+  endif()
+  if(PROJECT_NAME STREQUAL "Project")
+    message(FATAL_ERROR "catkin_metapackage() PROJECT_NAME is set to 'Project', which is not a valid project name. You must call project() before calling catkin_metapackage().")
+  endif()
+
+  cmake_parse_arguments(PROJECT "" "" "INCLUDE_DIRS;LIBRARIES;CATKIN_DEPENDS;DEPENDS;CFG_EXTRAS" ${ARGN})
+  if(PROJECT_UNPARSED_ARGUMENTS)
+    message(FATAL_ERROR "catkin_package() called with unused arguments: ${PROJECT_UNPARSED_ARGUMENTS}")
+  endif()
+
+  # call catkin_package_xml()
+  if(${CMAKE_CURRENT_LIST_FILE} STREQUAL ${CMAKE_BINARY_DIR}/catkin_generated/metapackages/${PROJECT_NAME}/CMakeLists.txt)
+    set(package_dir ${CMAKE_SOURCE_DIR}/${path})
+    catkin_package_xml(DIRECTORY ${CMAKE_SOURCE_DIR}/${path})
+  else()
+    set(package_dir ${CMAKE_CURRENT_SOURCE_DIR})
+    catkin_package_xml()
+  endif()
+
+  # install package.xml
+  install(FILES ${package_dir}/package.xml
+    DESTINATION share/${PROJECT_NAME}
+  )
+endfunction()

cmake/templates/metapackage.cmake.in
@@ -1,0 +1,4 @@
+cmake_minimum_required(VERSION 2.8.3)
+project(@name@)
+find_package(catkin REQUIRED)
+catkin_metapackage()

python/catkin/builder.py
@@ -68,7 +68,6 @@ def colorize_line(line):
         cline = cline.replace('~~ ', '@{pf}~~ @|')
         cline = cline.replace(' - ', ' - @!@{bf}')
         cline = cline.replace('(', '@|(')
-        cline = cline.replace('(metapackage)', '@|(@{cf}metapackage@|)')
         cline = cline.replace('(plain cmake)', '@|(@{rf}plain cmake@|)')
         cline = cline.replace('(unknown)', '@|(@{yf}unknown@|)')
     if line.startswith('-- +++'):
@@ -76,7 +75,6 @@ def colorize_line(line):
         cline = cline.replace('+++', '@!@{gf}+++@|')
         cline = cline.replace('kin package: \'', 'kin package: \'@!@{bf}')
         cline = cline.replace(')', '@|)')
-        cline = cline.replace('metapackage: \'', 'metapackage: \'@!@{bf}')
         cline = cline.replace('\'\n', '@|\'\n')
         cline = cline.replace('cmake package: \'', 'cmake package: \'@!@{bf}')
         cline = cline.replace('\'\n', '@|\'\n')
@@ -214,6 +212,10 @@ def build_catkin_package(
     # Check for Makefile and maybe call cmake
     makefile = os.path.join(build_dir, 'Makefile')
     if not os.path.exists(makefile) or force_cmake:
+        if not os.path.exists(os.path.join(os.path.dirname(package.filename), 'CMakeLists.txt')):
+            print(colorize_line('Error: Package "%s" does not have a CMakeLists.txt file' % package.name))
+            sys.exit('Can not build catkin package without CMakeLists.txt file')
+
         # Run cmake
         cmake_cmd = [
             'cmake',
@@ -375,34 +377,30 @@ def build_package(
     install, jobs, force_cmake, quiet, last_env, cmake_args,
     number=None, of=None
 ):
-    export_tags = [e.tagname for e in package.exports]
     cprint('@!@{gf}==>@| ', end='')
     new_last_env = get_new_env(package, develspace, installspace, install, last_env)
-    if 'metapackage' in export_tags:
-        cprint("Skipping @!metapackage@|: '@!@{bf}" + package.name + "@|'")
+    build_type = _get_build_type(package)
+    if build_type == 'catkin':
+        build_catkin_package(
+            path, package,
+            workspace, buildspace, develspace, installspace,
+            install, jobs, force_cmake, quiet, last_env, cmake_args
+        )
+        if not os.path.exists(new_last_env):
+            raise RuntimeError(
+                "No env.sh file generated at: '" + new_last_env +
+                "'\n  This sometimes occurs when a non-catkin package is "
+                "interpreted as a catkin package.\n  This can also occur "
+                "when the cmake cache is stale, try --force-cmake."
+            )
+    elif build_type == 'cmake':
+        build_cmake_package(
+            path, package,
+            workspace, buildspace, develspace, installspace,
+            install, jobs, force_cmake, quiet, last_env, cmake_args
+        )
     else:
-        build_type = _get_build_type(package)
-        if build_type == 'catkin':
-            build_catkin_package(
-                path, package,
-                workspace, buildspace, develspace, installspace,
-                install, jobs, force_cmake, quiet, last_env, cmake_args
-            )
-            if not os.path.exists(new_last_env):
-                raise RuntimeError(
-                    "No env.sh file generated at: '" + new_last_env +
-                    "'\n  This sometimes occurs when a non-catkin package is "
-                    "interpreted as a catkin package.\n  This can also occur "
-                    "when the cmake cache is stale, try --force-cmake."
-                )
-        elif build_type == 'cmake':
-            build_cmake_package(
-                path, package,
-                workspace, buildspace, develspace, installspace,
-                install, jobs, force_cmake, quiet, last_env, cmake_args
-            )
-        else:
-            sys.exit('Can not build package with unknown build_type')
+        sys.exit('Can not build package with unknown build_type')
     if number is not None and of is not None:
         msg = ' [@{gf}@!' + str(number) + '@| of @!@{gf}' + str(of) + '@|]'
     else:
@@ -414,16 +412,12 @@ def build_package(
 
 def get_new_env(package, develspace, installspace, install, last_env):
     new_env = None
-    export_tags = [e.tagname for e in package.exports]
-    if 'metapackage' in export_tags:
-        new_env = last_env
-    else:
-        build_type = _get_build_type(package)
-        if build_type in ['catkin', 'cmake']:
-            new_env = os.path.join(
-                installspace if install else develspace,
-                'env.sh'
-            )
+    build_type = _get_build_type(package)
+    if build_type in ['catkin', 'cmake']:
+        new_env = os.path.join(
+            installspace if install else develspace,
+            'env.sh'
+        )
     return new_env
 
 
@@ -530,15 +524,12 @@ def build_workspace_isolated(
     if not packages:
         sys.exit("No packages found in source space: {0}".format(sourcespace))
 
-    # verify that specified package exists in workspace and is not a metapackage
+    # verify that specified package exists in workspace
     if build_packages:
         packages_by_name = {p.name: path for path, p in packages.iteritems()}
         unknown_packages = [p for p in build_packages if p not in packages_by_name]
         if unknown_packages:
             sys.exit('Packages not found in the workspace: %s' % ', '.join(unknown_packages))
-        metapackages = [p for p in build_packages if ('metapackage' in [e.tagname for e in packages[packages_by_name[p]].exports])]
-        if metapackages:
-            sys.exit('Packages are metapackages and can not be built: %s' % ', '.join(metapackages))
 
     # Report topological ordering
     ordered_packages = topological_order_packages(packages)
@@ -548,30 +539,24 @@ def build_workspace_isolated(
     msg.append('@{pf}~~@|  traversing packages in topological order:')
     for path, package in ordered_packages:
         export_tags = [e.tagname for e in package.exports]
-        if 'metapackage' in export_tags:
+        if 'build_type' in export_tags:
+            build_type_tag = [e.content for e in package.exports
+                                        if e.tagname == 'build_type'][0]
+        else:
+            build_type_tag = 'catkin'
+        if build_type_tag == 'catkin':
+            msg.append('@{pf}~~@|  - @!@{bf}' + package.name + '@|')
+        elif build_type_tag == 'cmake':
             msg.append(
                 '@{pf}~~@|  - @!@{bf}' + package.name + '@|' +
-                ' (@{cf}metapackage@|)'
+                ' (@!@{cf}plain cmake@|)'
             )
         else:
-            if 'build_type' in export_tags:
-                build_type_tag = [e.content for e in package.exports
-                                            if e.tagname == 'build_type'][0]
-            else:
-                build_type_tag = 'catkin'
-            if build_type_tag == 'catkin':
-                msg.append('@{pf}~~@|  - @!@{bf}' + package.name + '@|')
-            elif build_type_tag == 'cmake':
-                msg.append(
-                    '@{pf}~~@|  - @!@{bf}' + package.name + '@|' +
-                    ' (@!@{cf}plain cmake@|)'
-                )
-            else:
-                msg.append(
-                    '@{pf}~~@|  - @!@{bf}' + package.name + '@|' +
-                    ' (@{rf}unknown@|)'
-                )
-                unknown_build_types.append(package)
+            msg.append(
+                '@{pf}~~@|  - @!@{bf}' + package.name + '@|' +
+                ' (@{rf}unknown@|)'
+            )
+            unknown_build_types.append(package)
     msg.append('@{pf}~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
     for index in range(len(msg)):
         msg[index] = fmt(msg[index])

commit cbe6a0ce3ac6d13914d3b4b010148f72e7984149

fix check if metapackage has CMakeLists (regression from cb5564734d5b4ae3284b109b286fc4c543bb5493)

cmake/catkin_workspace.cmake
@@ -52,11 +52,11 @@ function(catkin_workspace)
         configure_file(${catkin_EXTRAS_DIR}/templates/metapackage.cmake.in
           ${CMAKE_CURRENT_BINARY_DIR}/catkin_generated/metapackages/${name}/CMakeLists.txt
           @ONLY)
-        if(EXISTS ${path}/CMakeLists.txt)
+        if(EXISTS ${CMAKE_SOURCE_DIR}/${path}/CMakeLists.txt)
           # compare CMakeLists.txt with standard content
           file(STRINGS ${CMAKE_CURRENT_BINARY_DIR}/catkin_generated/metapackages/${name}/CMakeLists.txt generated_cmakelists)
           file(STRINGS ${path}/CMakeLists.txt existing_cmakelists)
-          if(NOT ${generated_cmakelists} STREQUAL ${existing_cmakelists})
+          if(NOT "${generated_cmakelists}" STREQUAL "${existing_cmakelists}")
             set(CATKIN_NONHOMOGENEOUS_WORKSPACE TRUE)
             message("WARNING: The CMakeLists.txt of the metapackage '${name}' contains non standard content. Use the content of the following file instead: ${CMAKE_CURRENT_BINARY_DIR}/catkin_generated/metapackages/${name}/CMakeLists.txt")
           endif()
@@ -93,7 +93,7 @@ function(catkin_workspace)
       list(GET CATKIN_ORDERED_PACKAGES_BUILD_TYPE ${index} build_type)
       if(${is_meta})
         message(STATUS "+++ processing catkin metapackage: '${name}'")
-        if(EXISTS ${path}/CMakeLists.txt)
+        if(EXISTS ${CMAKE_SOURCE_DIR}/${path}/CMakeLists.txt)
           message(STATUS "==> add_subdirectory(${path})")
           add_subdirectory(${path})
         else()