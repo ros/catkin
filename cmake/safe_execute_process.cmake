31f4190943ed4a492425ad7f190f1f05547e7e77
Dirk Thomas <dthomas@willowgarage.com>
2013-03-04 17:49:35 -0800

    fix handling spaces in folder names (ros/catkin#375)

--- cmake/safe_execute_process.cmake
+++ cmake/safe_execute_process.cmake
@@ -1,7 +1,7 @@
 macro(safe_execute_process cmd_keyword arg1)
   set(_cmd ${arg1})
   foreach(_arg ${ARGN})
-    set(_cmd "${_cmd} ${_arg}")
+    set(_cmd "${_cmd} \"${_arg}\"")
   endforeach()
 
   debug_message(2 "execute_process(${_cmd})")
