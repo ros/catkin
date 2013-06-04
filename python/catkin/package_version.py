020b6bf35e45877d0329486daf7bdc57017773da
Dirk Thomas <dthomas@osrfoundation.org>
2013-06-04 11:31:31 -0700

    remove bump_version() after moving to catkin_pkg (ros-infrastructure/catkin_pkg#44

--- python/catkin/package_version.py
+++ python/catkin/package_version.py
@@ -35,29 +35,6 @@ import os
 import re
 
 
-def bump_version(version, bump='patch'):
-    """
-    Increases version number.
-
-    :param version: str, must be in version format "int.int.int"
-    :param bump: str, one of 'patch, minor, major'
-    :returns: version with the given part increased, and all inferior parts reset to 0
-    """
-    # split the version number
-    match = re.match('^(\d+)\.(\d+)\.(\d+)$', version)
-    if match is None:
-        raise ValueError('Invalid version string, must be int.int.int: "%s"' % version)
-    new_version = match.groups()
-    new_version = [int(x) for x in new_version]
-    # find the desired index
-    idx = dict(major=0, minor=1, patch=2)[bump]
-    # increment the desired part
-    new_version[idx] += 1
-    # reset all parts behind the bumped part
-    new_version = new_version[:idx + 1] + [0 for x in new_version[idx + 1:]]
-    return '%d.%d.%d' % tuple(new_version)
-
-
 def _replace_version(package_str, new_version):
     """
     replaces the version tag in contents if there is only one instance
--- test/unit_tests/test_package_version.py
+++ test/unit_tests/test_package_version.py
@@ -4,31 +4,13 @@ import tempfile
 import shutil
 
 try:
-    from catkin.package_version import bump_version, _replace_version, update_versions
+    from catkin.package_version import _replace_version, update_versions
 except ImportError as impe:
     raise ImportError(
         'Please adjust your pythonpath before running this test: %s' % str(impe))
 
 
 class PackageVersionTest(unittest.TestCase):
-
-    def test_bump_version(self):
-        self.assertEqual('0.0.1', bump_version('0.0.0'))
-        self.assertEqual('1.0.1', bump_version('1.0.0'))
-        self.assertEqual('0.1.1', bump_version('0.1.0'))
-        self.assertEqual('0.0.1', bump_version('0.0.0', 'patch'))
-        self.assertEqual('1.0.1', bump_version('1.0.0', 'patch'))
-        self.assertEqual('0.1.1', bump_version('0.1.0', 'patch'))
-        self.assertEqual('1.0.0', bump_version('0.0.0', 'major'))
-        self.assertEqual('1.0.0', bump_version('0.0.1', 'major'))
-        self.assertEqual('1.0.0', bump_version('0.1.1', 'major'))
-        self.assertEqual('0.1.0', bump_version('0.0.0', 'minor'))
-        self.assertEqual('0.1.0', bump_version('0.0.1', 'minor'))
-        self.assertEqual('1.1.0', bump_version('1.0.1', 'minor'))
-        self.assertRaises(ValueError, bump_version, '0.0.asd')
-        self.assertRaises(ValueError, bump_version, '0.0')
-        self.assertRaises(ValueError, bump_version, '0')
-        self.assertRaises(ValueError, bump_version, '0.0.-1')
 
     def test_replace_version(self):
         self.assertEqual("<package><version>0.1.1</version></package>",
