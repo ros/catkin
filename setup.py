fa4bd33f88b75a25a4fcfd5f8af124bb9afdcd03
Dirk Thomas <dthomas@osrfoundation.org>
2013-12-12 13:35:31 -0800

    install scripts under bin via setup.py (#555)

--- setup.py
+++ setup.py
@@ -5,7 +5,17 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
     packages=['catkin'],
-    package_dir={'': 'python'}
+    package_dir={'': 'python'},
+    scripts=[
+        'bin/catkin_find',
+        'bin/catkin_init_workspace',
+        'bin/catkin_make',
+        'bin/catkin_make_isolated',
+        'bin/catkin_package_version',
+        'bin/catkin_prepare_release',
+        'bin/catkin_test_results',
+        'bin/catkin_topological_order',
+    ],
 )
 
 setup(**d)
