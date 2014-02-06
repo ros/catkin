31c0bbd141f7353ca1a68ecc38923b40ae6eaa1c
Dirk Thomas <dthomas@osrfoundation.org>
2014-02-06 11:28:38 +0100

    update unit tests to use _setup_util.py.in

--- test/unit_tests/test_setup_util.py
+++ test/unit_tests/test_setup_util.py
@@ -1,26 +1,36 @@
 import os
-import unittest
-import tempfile
 import shutil
+import tempfile
+import unittest
 
-import imp
-imp.load_source('setup_util',
-                os.path.join(os.path.dirname(__file__),
-                             '..', '..', 'cmake', 'templates', '_setup_util.py'))
+from catkin_pkg.cmake import configure_file
+
+data = configure_file(os.path.join(os.path.dirname(__file__), '..', '..', 'cmake', 'templates', '_setup_util.py.in'),
+                      {
+                          'CATKIN_GLOBAL_LIB_DESTINATION': 'lib',
+                          'CATKIN_GLOBAL_BIN_DESTINATION': 'bin',
+                          'PYTHON_INSTALL_DIR': 'pythonX.Y/packages',
+                          'CMAKE_PREFIX_PATH_AS_IS': '',
+                      })
+with tempfile.TemporaryFile() as setup_util_file:
+    setup_util_file.write(data)
+    setup_util_file.seek(0)
+
+    import imp
+    imp.load_source('setup_util', '/somewhere/_setup_util.py', setup_util_file)
 
 import setup_util
-from setup_util import get_reversed_workspaces, prefix_env, remove_from_env, CATKIN_MARKER_FILE
+from setup_util import _get_workspaces, _prefix_env_variable, _rollback_env_variable, CATKIN_MARKER_FILE
 
 
 class SetupUtilTest(unittest.TestCase):
 
     def test_get_reversed_workspaces(self):
         try:
-            mock_env = {}
             rootdir = tempfile.mkdtemp()
-            setup_util.os.environ = mock_env
-            self.assertEqual('', get_reversed_workspaces())
-            self.assertEqual('', get_reversed_workspaces('foo'))
+            mock_env = {}
+            self.assertEqual([], _get_workspaces(mock_env))
+            self.assertEqual([], _get_workspaces(mock_env, 'foo'))
             foows = os.path.join(rootdir, 'foo')
             os.makedirs(foows)
             with open(os.path.join(foows, CATKIN_MARKER_FILE), 'w') as fhand:
@@ -31,40 +41,32 @@ class SetupUtilTest(unittest.TestCase):
                 fhand.write('')
             nows = os.path.join(rootdir, 'nows')
             os.makedirs(nows)
+            mock_env = {'CMAKE_PREFIX_PATH': foows}
+            self.assertEqual([foows], _get_workspaces(mock_env))
             mock_env = {'CMAKE_PREFIX_PATH': os.pathsep.join([nows, foows, barws, 'invalid'])}
-            setup_util.os.environ = mock_env
-            self.assertEqual('\n'.join([barws, foows]), get_reversed_workspaces())
-            self.assertEqual(barws, get_reversed_workspaces(foows))
-            self.assertEqual(foows, get_reversed_workspaces(barws))
+            self.assertEqual([foows, barws], _get_workspaces(mock_env))
         finally:
             shutil.rmtree(rootdir)
-            setup_util.os.environ = os.environ
 
     def test_prefix_env(self):
-        try:
-            mock_env = {}
-            setup_util.os.environ = mock_env
-            self.assertEqual('',
-                             prefix_env('varname', ''))
-            self.assertEqual(os.pathsep.join(['foo', 'bar']),
-                             prefix_env('varname', 'foo:bar'))
-            mock_env = {'varname': os.pathsep.join(['baz', 'bar', 'bam'])}
-            setup_util.os.environ = mock_env
-            self.assertEqual('',
-                             prefix_env('varname', ''))
-            self.assertEqual('foo' + os.pathsep,
-                             prefix_env('varname', 'foo:bar'))
-            self.assertEqual(os.pathsep.join(['foo', 'lim']) + os.pathsep,
-                             prefix_env('varname', 'foo:lim:foo:lim'))
-        finally:
-            setup_util.os.environ = os.environ
+        mock_env = {}
+        self.assertEqual('',
+                         _prefix_env_variable(mock_env, 'varname', [], ''))
+        self.assertEqual(os.pathsep.join(['foo', 'bar']),
+                         _prefix_env_variable(mock_env, 'varname', ['foo', 'bar'], ''))
+        mock_env = {'varname': os.pathsep.join(['baz', 'bar', 'bam'])}
+        self.assertEqual('',
+                         _prefix_env_variable(mock_env, 'varname', [], ''))
+        self.assertEqual('foo' + os.pathsep,
+                         _prefix_env_variable(mock_env, 'varname', ['foo', 'bar'], ''))
+        self.assertEqual(os.pathsep.join(['foo', 'lim']) + os.pathsep,
+                         _prefix_env_variable(mock_env, 'varname', ['foo', 'lim', 'foo', 'lim'], ''))
 
     def test_remove_from_env(self):
         altsep = os.path.altsep
         try:
-            mock_env = {}
             rootdir = tempfile.mkdtemp()
-            setup_util.os.environ = mock_env
+            mock_env = {}
             # foows
             foows = os.path.join(rootdir, 'foo')
             foolib = os.path.join(foows, 'lib') + '/'
@@ -82,23 +84,21 @@ class SetupUtilTest(unittest.TestCase):
             wsvarname = 'workspaces'
             mock_env = {varname: os.pathsep.join([foolib, barlib]),
                         'CMAKE_PREFIX_PATH': barws}
-            setup_util.os.environ = mock_env
             # since workspace foo is not in CMAKE_PREFIX_PATH, it remains in varname
-            self.assertEqual(foolib, remove_from_env(varname, '/lib'))
+            self.assertEqual(foolib, _rollback_env_variable(mock_env, varname, '/lib'))
 
             # mock_env with both ws in CPP
             mock_env = {varname: os.pathsep.join([foolib, barlib]),
                         wsvarname: os.pathsep.join([foows, barws]),
                         'CMAKE_PREFIX_PATH': os.pathsep.join([foows, barws])}
-            setup_util.os.environ = mock_env
 
-            self.assertEqual(os.pathsep.join([foolib, barlib]), remove_from_env(varname, ''))
-            self.assertEqual(os.pathsep.join([foolib, barlib]), remove_from_env(varname, 'nolib'))
-            self.assertEqual(os.pathsep.join([foolib, barlib]), remove_from_env(varname, '/nolib'))
-            self.assertEqual('', remove_from_env(varname, 'lib'))
-            self.assertEqual('', remove_from_env(varname, '/lib'))
-            self.assertEqual(os.pathsep.join([foolib, barlib]), remove_from_env(varname, ''))
-            self.assertEqual('', remove_from_env(wsvarname, ''))
+            self.assertEqual(None, _rollback_env_variable(mock_env, varname, ''))
+            self.assertEqual(None, _rollback_env_variable(mock_env, varname, 'nolib'))
+            self.assertEqual(None, _rollback_env_variable(mock_env, varname, '/nolib'))
+            self.assertEqual('', _rollback_env_variable(mock_env, varname, 'lib'))
+            self.assertEqual('', _rollback_env_variable(mock_env, varname, '/lib'))
+            self.assertEqual(None, _rollback_env_variable(mock_env, varname, ''))
+            self.assertEqual('', _rollback_env_variable(mock_env, wsvarname, ''))
 
             # nows: not a workspace
             nows = os.path.join(rootdir, 'nows')
@@ -108,15 +108,13 @@ class SetupUtilTest(unittest.TestCase):
 
             mock_env = {'varname': os.pathsep.join([foolib, nowslib, barlib, foolib]),
                         'CMAKE_PREFIX_PATH': os.pathsep.join([foows, barws])}
-            setup_util.os.environ = mock_env
             # checks nows/lib remains, and second mention of foolib
-            self.assertEqual(os.pathsep.join([nowslib, foolib]), remove_from_env('varname', '/lib'))
-            self.assertEqual(os.pathsep.join([nowslib, foolib]), remove_from_env('varname', 'lib'))
+            self.assertEqual(os.pathsep.join([nowslib, foolib]), _rollback_env_variable(mock_env, 'varname', '/lib'))
+            self.assertEqual(os.pathsep.join([nowslib, foolib]), _rollback_env_variable(mock_env, 'varname', 'lib'))
 
             # windows pathsep
             os.path.altsep = '\\'
-            self.assertEqual(os.pathsep.join([nowslib, foolib]), remove_from_env('varname', '\\lib'))
+            self.assertEqual(os.pathsep.join([nowslib, foolib]), _rollback_env_variable(mock_env, 'varname', '\\lib'))
         finally:
-            setup_util.os.environ = os.environ
             os.path.altsep = altsep
             shutil.rmtree(rootdir)
