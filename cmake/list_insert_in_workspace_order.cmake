cde4e0e6a0884e66c1b13a5cfa404e1e937df002
Max Schwarz <max.schwarz@online.de>
2014-02-15 17:49:07 +0100

    implement list_insert_in_workspace in plain CMake without python
    This speeds up the CMake run from 12s to 8s on a workspace with ~50
    packages.

--- cmake/list_insert_in_workspace_order.cmake
+++ cmake/list_insert_in_workspace_order.cmake
@@ -12,23 +12,45 @@ foreach(_space ${CATKIN_DEVEL_PREFIX} ${CATKIN_WORKSPACES})
   list(APPEND CATKIN_ORDERED_SPACES ${_sourcespaces})
 endforeach()
 
+# CATKIN_ORDERED_SPACES_END is the last index in the workspaces list,
+# which includes an empty prefix at the end for paths which do not belong
+# in a known workspace.
+list(LENGTH CATKIN_ORDERED_SPACES CATKIN_ORDERED_SPACES_END)
+list(APPEND CATKIN_ORDERED_SPACES "")
+
 debug_message(10 "CATKIN_ORDERED_SPACES ${CATKIN_ORDERED_SPACES}")
 
 macro(list_insert_in_workspace_order listname)
   if(NOT "${ARGN}" STREQUAL "")
-    assert(CATKIN_ENV)
-    assert(PYTHON_EXECUTABLE)
-    set(cmd
-      ${CATKIN_ENV} ${PYTHON_EXECUTABLE}
-      ${catkin_EXTRAS_DIR}/order_paths.py
-      ${${PROJECT_NAME}_BINARY_DIR}/catkin_generated/ordered_paths.cmake
-      --paths-to-order ${ARGN}
-      --prefixes ${CATKIN_ORDERED_SPACES}
-    )
-    debug_message(10 "list_insert_in_workspace_order() in project '{PROJECT_NAME}' executes:  ${cmd}")
-    safe_execute_process(COMMAND ${cmd})
-    include(${${PROJECT_NAME}_BINARY_DIR}/catkin_generated/ordered_paths.cmake)
-    set(${listname} ${ORDERED_PATHS})
+    # Init list of empty slots, one for each workspace and one with an
+    # empty prefix
+    foreach(i RANGE ${CATKIN_ORDERED_SPACES_END})
+      set(slot_${i} "")
+    endforeach()
+
+    # Sort the paths into the first matching workspace
+    foreach(path ${ARGN})
+      string(LENGTH "${path}" path_len)
+      foreach(i RANGE ${CATKIN_ORDERED_SPACES_END})
+        list(GET CATKIN_ORDERED_SPACES ${i} space)
+        string(LENGTH "${space}" len)
+
+        # If the path is long enough, compare the prefix
+        if(${len} LESS ${path_len})
+          string(SUBSTRING "${path}" 0 ${len} path_prefix)
+          if("${space}" STREQUAL "${path_prefix}")
+            list(APPEND slot_${i} "${path}")
+            break()
+          endif()
+        endif()
+      endforeach()
+    endforeach()
+
+    # Flatten the slot list
+    set(${listname} "")
+    foreach(i RANGE ${CATKIN_ORDERED_SPACES_END})
+        list(APPEND ${listname} ${slot_${i}})
+    endforeach()
   else()
     set(${listname} "")
   endif()
