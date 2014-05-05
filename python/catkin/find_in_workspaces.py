cb1e8febcc6cb819e25a9fc6d3235379ac97e5e1
Dirk Thomas <dthomas@osrfoundation.org>
2014-05-05 13:52:41 -0700

    allow better performance for repeated invocations of find_in_workspaces()

--- python/catkin/find_in_workspaces.py
+++ python/catkin/find_in_workspaces.py
@@ -87,7 +87,7 @@ def _get_valid_search_dirs(search_dirs, project):
 #      except for s == 'share', cand is a list of two paths: ws[0] + s + project (+ path) and ws[1] + project (+ path)
 #      add cand to result list if it exists
 #      is not defined for s in ['bin', 'lib'], bailing out
-def find_in_workspaces(search_dirs=None, project=None, path=None, _workspaces=get_workspaces(), considered_paths=None, first_matching_workspace_only=False, first_match_only=False):
+def find_in_workspaces(search_dirs=None, project=None, path=None, _workspaces=get_workspaces(), considered_paths=None, first_matching_workspace_only=False, first_match_only=False, workspace_to_source_spaces=None, source_path_to_packages=None):
     '''
     Find all paths which match the search criteria.
     All workspaces are searched in order.
@@ -102,10 +102,17 @@ def find_in_workspaces(search_dirs=None, project=None, path=None, _workspaces=ge
     :param considered_paths: If not None, function will append all path that were searched
     :param first_matching_workspace_only: if True returns all results found for first workspace with results
     :param first_match_only: if True returns first path found (supercedes first_matching_workspace_only)
+    :param workspace_to_source_spaces: the dictionary is populated with mappings from workspaces to source paths, pass in the same dictionary to avoid repeated reading of the catkin marker file
+    :param source_path_to_packages: the dictionary is populated with mappings from source paths to packages, pass in the same dictionary to avoid repeated crawling
     :raises ValueError: if search_dirs contains an invalid folder name
     :returns: List of paths
     '''
     search_dirs = _get_valid_search_dirs(search_dirs, project)
+
+    if workspace_to_source_spaces is None:
+        workspace_to_source_spaces = {}
+    if source_path_to_packages is None:
+        source_path_to_packages = {}
 
     paths = []
     existing_paths = []
@@ -126,10 +133,12 @@ def find_in_workspaces(search_dirs=None, project=None, path=None, _workspaces=ge
 
                 # for search in share also consider source spaces
                 if project is not None and sub == 'share':
-                    source_paths = get_source_paths(workspace)
-                    for source_path in source_paths:
-                        packages = find_packages(source_path)
-                        matching_packages = [p for p, pkg in packages.items() if pkg.name == project]
+                    if workspace not in workspace_to_source_spaces:
+                        workspace_to_source_spaces[workspace] = get_source_paths(workspace)
+                    for source_path in workspace_to_source_spaces[workspace]:
+                        if source_path not in source_path_to_packages:
+                            source_path_to_packages[source_path] = find_packages(source_path)
+                        matching_packages = [p for p, pkg in source_path_to_packages[source_path].items() if pkg.name == project]
                         if matching_packages:
                             p = source_path
                             if matching_packages[0] != os.curdir:
