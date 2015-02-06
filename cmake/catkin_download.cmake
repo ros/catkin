ec239b6df81b97809b0940e6c6ef64d227325386
Brice Rebsamen <brice.rebsamen@gmail.com>
2015-02-06 12:10:17 -0800

    some improvements to the download functions
    - addressed @dirk-thomas comments in #716:
      - corrected the doc for the default destination to match the code
      - REQUIRED and EXCLUDE_FROM_ALL as options instead of arguments
      - removed the extra functions catkin_download_build_data and
        catkin_download_extra_data
    - fixed a bug with the --ignore-error option to download_checkmd5.py
    - in catkin_download_test_data(), calling catkin_download() with the
      EXCLUDE_FROM_ALL option.

--- cmake/catkin_download.cmake
+++ cmake/catkin_download.cmake
@@ -17,7 +17,7 @@
 # :type url: string
 #
 # :param DESTINATION: the directory where the file is downloaded to
-#   (default: ${CATKIN_DEVEL_PREFIX}/${CATKIN_PACKAGE_SHARE_DESTINATION})
+#   (default: ${PROJECT_BINARY_DIR})
 # :type DESTINATION: string
 # :param FILENAME: the filename of the downloaded file
 #   (default: the basename of the url)
@@ -25,19 +25,16 @@
 # :param MD5: the expected md5 hash to compare against
 #   (default: empty, skipping the check)
 # :type MD5: string
-# :param EXCLUDE_FROM_ALL: exclude this download from the 'all' build target
-#   (default: true, excluding it from the all target)
-# :type EXCLUDE_FROM_ALL: bool
-# :param REQUIRED: require the file to be available for the build to succeed
-#   (default: true, requiring the file)
-# :type REQUIRED: bool
+#
+# Additionally, options EXCLUDE_FROM_ALL and REQUIRED can be specified.
 #
 # @public
 function(catkin_download target url)
-  cmake_parse_arguments(ARG ""
-    "DESTINATION;FILENAME;MD5;EXCLUDE_FROM_ALL;REQUIRED" "" ${ARGN})
+  cmake_parse_arguments(ARG "EXCLUDE_FROM_ALL;REQUIRED" 
+                        "DESTINATION;FILENAME;MD5" "" ${ARGN})
   if(ARG_UNPARSED_ARGUMENTS)
-    message(FATAL_ERROR "catkin_download() called with unused arguments: ${ARG_UNPARSED_ARGUMENTS}")
+    message(FATAL_ERROR 
+    "catkin_download() called with unused arguments: ${ARG_UNPARSED_ARGUMENTS}")
   endif()
 
   if(NOT ARG_DESTINATION)
@@ -49,97 +46,27 @@ function(catkin_download target url)
   endif()
 
   set(required "")
-  if(DEFINED ARG_REQUIRED)
-    if(NOT ARG_REQUIRED)
-      set(required "--non-essential")
-    endif()
+  if(NOT ARG_REQUIRED)
+    set(required "--ignore-error")
   endif()
 
   set(output "${ARG_DESTINATION}/${ARG_FILENAME}")
-
-  set(DOWNLOAD_CHECKMD5_SCRIPT "${catkin_EXTRAS_DIR}/test/download_checkmd5.py")
 
   # With this, the command is always called, even when the output is up to date.
   # this is because we want to check the md5 sum if it's given, and redownload
   # the target if the md5 sum does not match.
   add_custom_target(${target}
-    COMMAND ${DOWNLOAD_CHECKMD5_SCRIPT} ${url} ${output} ${ARG_MD5} ${required}
+    COMMAND ${PYTHON} ${catkin_EXTRAS_DIR}/test/download_checkmd5.py ${url} ${output} ${ARG_MD5} ${required}
     VERBATIM)
+
+  set_target_properties(${target} PROPERTIES EXCLUDE_FROM_ALL ${ARG_EXCLUDE_FROM_ALL})
 
   if(TARGET download_extra_data)
     add_dependencies(download_extra_data ${target})
   endif()
 
-  if(DEFINED ARG_EXCLUDE_FROM_ALL)
-    set_target_properties(${target} PROPERTIES
-      EXCLUDE_FROM_ALL ${ARG_EXCLUDE_FROM_ALL})
-  endif()
-
 endfunction()
 
-#
-# Download a file containing data from a URL.
-#
-# It is commonly used to download larger data files which should not be
-# stored in the repository.
-#
-# .. note:: The target will be registered as a dependency
-#   of the "download_extra_data" target and the all target
-#
-# :param target: the target name
-# :type target: string
-# :param url: the url to download
-# :type url: string
-#
-# :param DESTINATION: the directory where the file is downloaded to
-#   (default: ${CATKIN_DEVEL_PREFIX}/${CATKIN_PACKAGE_SHARE_DESTINATION})
-# :type DESTINATION: string
-# :param FILENAME: the filename of the downloaded file
-#   (default: the basename of the url)
-# :type FILENAME: string
-# :param MD5: the expected md5 hash to compare against
-#   (default: empty, skipping the check)
-# :type MD5: string
-# :param REQUIRED: require the file to be available for the build to succeed
-#   (default: true, requiring the file)
-# :type REQUIRED: bool
-#
-# @public
-function(catkin_download_build_data)
-  catkin_download(${ARGN} EXCLUDE_FROM_ALL false)
-endfunction()
-
-#
-# Download a file containing data from a URL.
-#
-# It is commonly used to download larger data files which should not be
-# stored in the repository, and which are not required as part of the build
-#
-# .. note:: The target will be registered as a dependency
-#   of the "download_extra_data" target
-#
-# :param target: the target name
-# :type target: string
-# :param url: the url to download
-# :type url: string
-#
-# :param DESTINATION: the directory where the file is downloaded to
-#   (default: ${CATKIN_DEVEL_PREFIX}/${CATKIN_PACKAGE_SHARE_DESTINATION})
-# :type DESTINATION: string
-# :param FILENAME: the filename of the downloaded file
-#   (default: the basename of the url)
-# :type FILENAME: string
-# :param MD5: the expected md5 hash to compare against
-#   (default: empty, skipping the check)
-# :type MD5: string
-# :param REQUIRED: require the file to be available for the build to succeed
-#   (default: true, requiring the file)
-# :type REQUIRED: bool
-#
-# @public
-function(catkin_download_extra_data)
-  catkin_download(${ARGN} EXCLUDE_FROM_ALL true)
-endfunction()
 
 if(NOT TARGET download_extra_data)
   add_custom_target(download_extra_data)
--- cmake/test/catkin_download_test_data.cmake
+++ cmake/test/catkin_download_test_data.cmake
@@ -6,8 +6,12 @@ _generate_function_if_testing_is_disabled("catkin_download_test_data")
 # It is commonly used to download larger data files for unit tests
 # which should not be stored in the repository.
 #
+# .. note:: It is not recommended to rely on downloaded data during
+#   a configure / make cycle since this prevents building the package
+#   when no network connectivity is available.
+#
 # .. note:: The target will be registered as a dependency
-#   of the "tests" target.
+#   of the "tests" and "download_extra_data" targets, but not of "all" target.
 #
 # .. note:: If the tests should be run on the ROS buildfarm the URL
 #   must be publically and reliably accessible.
@@ -27,11 +31,13 @@ _generate_function_if_testing_is_disabled("catkin_download_test_data")
 #   (default: empty, skipping the check)
 # :type MD5: string
 #
+# Additionally, option REQUIRED can be specified.
+#
 # @public
 function(catkin_download_test_data target url)
   _warn_if_skip_testing("catkin_download_test_data")
 
-  catkin_download("${target}" "${url}" ${ARGN})
+  catkin_download("${target}" "${url}" ${ARGN} EXCLUDE_FROM_ALL)
 
   if(TARGET tests)
     add_dependencies(tests ${target})
