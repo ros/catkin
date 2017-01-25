56ef1abea083066fe680b80487dccf84dd3f47b6
Dirk Thomas <dthomas@osrfoundation.org>
2017-01-25 15:56:16 -0800

    maintain public API

--- test/unit_tests/test_test_results.py
+++ test/unit_tests/test_test_results.py
@@ -26,9 +26,11 @@ class TestResultsTest(unittest.TestCase):
 
             result_file = os.path.join(rootdir, 'test1.xml')
             with open(result_file, 'w') as fhand:
-                fhand.write('<testsuites tests="5" failures="3" errors="1" disabled="1" time="35" name="AllTests"></testsuites>')
-            (num_tests, num_errors, num_failures, num_skipped) = catkin_test_results.read_junit(result_file)
-            self.assertEqual((5, 1, 3, 1), (num_tests, num_errors, num_failures, num_skipped))
+                fhand.write('<testsuites tests="5" failures="3" errors="1" disabled="2" time="35" name="AllTests"></testsuites>')
+            (num_tests, num_errors, num_failures) = catkin_test_results.read_junit(result_file)
+            self.assertEqual((5, 1, 3), (num_tests, num_errors, num_failures))
+            (num_tests, num_errors, num_failures, num_skipped) = catkin_test_results.read_junit2(result_file)
+            self.assertEqual((5, 1, 3, 2), (num_tests, num_errors, num_failures, num_skipped))
         finally:
             shutil.rmtree(rootdir)
 
@@ -38,9 +40,11 @@ class TestResultsTest(unittest.TestCase):
 
             result_file = os.path.join(rootdir, 'test1.xml')
             with open(result_file, 'w') as fhand:
-                fhand.write('<testsuites tests="5" failures="3" errors="1" skip="1" time="35" name="AllTests"></testsuites>')
-            (num_tests, num_errors, num_failures, num_skipped) = catkin_test_results.read_junit(result_file)
-            self.assertEqual((5, 1, 3, 1), (num_tests, num_errors, num_failures, num_skipped))
+                fhand.write('<testsuites tests="5" failures="3" errors="1" skip="2" time="35" name="AllTests"></testsuites>')
+            (num_tests, num_errors, num_failures) = catkin_test_results.read_junit(result_file)
+            self.assertEqual((5, 1, 3), (num_tests, num_errors, num_failures))
+            (num_tests, num_errors, num_failures, num_skipped) = catkin_test_results.read_junit2(result_file)
+            self.assertEqual((5, 1, 3, 2), (num_tests, num_errors, num_failures, num_skipped))
         finally:
             shutil.rmtree(rootdir)
 
@@ -51,9 +55,11 @@ class TestResultsTest(unittest.TestCase):
             for filename in ['test1.xml', 'test2.xml', 'foo.bar']:
                 result_file = os.path.join(rootdir, filename)
                 with open(result_file, 'w') as fhand:
-                    fhand.write('<testsuites tests="5" failures="3" errors="1" disabled="1" time="35" name="AllTests"></testsuites>')
+                    fhand.write('<testsuites tests="5" failures="3" errors="1" disabled="2" time="35" name="AllTests"></testsuites>')
             results = catkin_test_results.test_results(rootdir)
-            self.assertEqual({'test1.xml': (5, 1, 3, 1), 'test2.xml': (5, 1, 3, 1)}, results)
+            self.assertEqual({'test1.xml': (5, 1, 3), 'test2.xml': (5, 1, 3)}, results)
+            results = catkin_test_results.test_results2(rootdir)
+            self.assertEqual({'test1.xml': (5, 1, 3, 2), 'test2.xml': (5, 1, 3, 2)}, results)
         finally:
             shutil.rmtree(rootdir)
 
@@ -68,7 +74,7 @@ class TestResultsTest(unittest.TestCase):
             with open(result_file, 'w') as fhand:
                 fhand.write(test_suites)
             results = catkin_test_results.test_results(rootdir, show_verbose=True)
-            self.assertEqual({test_xml: (5, 1, 3, 0)}, results)
+            self.assertEqual({test_xml: (5, 1, 3)}, results)
             summary = sys.stdout.getvalue()
             self.assertTrue(test_xml in summary, summary)
             self.assertTrue(test_suites in summary, summary)
@@ -87,7 +93,7 @@ class TestResultsTest(unittest.TestCase):
             with open(result_file, 'w') as fhand:
                 fhand.write(test_suites)
             results = catkin_test_results.test_results(rootdir, show_verbose=True)
-            self.assertEqual({test_xml: (5, 1, 3, 0)}, results)
+            self.assertEqual({test_xml: (5, 1, 3)}, results)
             summary = sys.stdout.getvalue()
             self.assertTrue(test_xml in summary, summary)
             self.assertTrue(test_suites in summary, summary)
@@ -97,15 +103,22 @@ class TestResultsTest(unittest.TestCase):
             print(summary)
 
     def test_print_summary(self):
-        results = {'test1.xml': (5, 1, 3, 1), 'test2.xml': (7, 2, 4, 1)}
+        results = {'test1.xml': (5, 1, 3, 2), 'test2.xml': (7, 2, 4, 1)}
         try:
             oldstdout = sys.stdout
             sys.stdout = StringIO()
             catkin_test_results.print_summary(results)
             summary = sys.stdout.getvalue()
-            self.assertTrue('5 tests, 1 errors, 3 failures, 1 skipped' in summary, summary)
+            self.assertTrue('5 tests, 1 errors, 3 failures' in summary, summary)
+            self.assertTrue('7 tests, 2 errors, 4 failures' in summary, summary)
+            self.assertTrue('12 tests, 3 errors, 7 failures' in summary, summary)
+
+            sys.stdout = StringIO()
+            catkin_test_results.print_summary2(results)
+            summary = sys.stdout.getvalue()
+            self.assertTrue('5 tests, 1 errors, 3 failures, 2 skipped' in summary, summary)
             self.assertTrue('7 tests, 2 errors, 4 failures, 1 skipped' in summary, summary)
-            self.assertTrue('12 tests, 3 errors, 7 failures, 2 skipped' in summary, summary)
+            self.assertTrue('12 tests, 3 errors, 7 failures, 3 skipped' in summary, summary)
 
         finally:
             sys.stdout = oldstdout
