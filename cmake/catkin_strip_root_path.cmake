941c20c4bacd0611673bbe252f35e144ab4bdd88
Dominique Hunziker <dominique.hunziker@rapyuta-robotics.com>
2017-06-03 08:13:25 +0200

    address review comments for catkin_strip_root_path

--- cmake/catkin_strip_root_path.cmake
+++ cmake/catkin_strip_root_path.cmake
@@ -3,26 +3,30 @@
 #
 # .. note:: Used for cross-compilation.
 #
-# :param var: the list to strip
-# :type var: list of paths
+# :param var: the input / output variable name
+# :type var: string
 # :param UNIQUE: if specified the stripped list will contain unique paths, i.e.,
 #   duplicate paths are not appended a second time
 # :type UNIQUE: option
 #
 function(catkin_strip_root_path var)
   cmake_parse_arguments(ARG "UNIQUE" "" "" ${ARGN})
-  set(_list)
-  foreach(_ele ${${var}})
-    if(IS_ABSOLUTE ${_ele})
-      foreach(_path ${CMAKE_FIND_ROOT_PATH})
-        string(REGEX REPLACE "^${_path}/" "/" _ele ${_ele})
+  set(output)
+  foreach(element ${${var}})
+    if(IS_ABSOLUTE ${element})
+      foreach(root_path ${CMAKE_FIND_ROOT_PATH})
+        if(element MATCHES "^${root_path}/")
+          string(LENGTH ${root_path} root_path_length)
+          string(SUBSTRING ${element} ${root_path_length} -1 element)
+          break()
+        endif()
       endforeach()
     endif()
     if(ARG_UNIQUE)
-      list_append_unique(_list ${_ele})
+      list_append_unique(output ${element})
     else()
-      list(APPEND _list ${_ele})
+      list(APPEND output ${element})
     endif()
   endforeach()
-  set(${var} ${_list} PARENT_SCOPE)
+  set(${var} ${output} PARENT_SCOPE)
 endfunction()
