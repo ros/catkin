e131753b03049f576d820111333b0cb624996c44
Kentaro Wada <www.kentaro.wada@gmail.com>
2017-08-08 01:32:50 +0900

    Revert "Use setuptools to install egg_info"
    This reverts commit 15689d55d08dc2b7c5e9815c4460085c3f709ddf.

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from setuptools import setup
+from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
