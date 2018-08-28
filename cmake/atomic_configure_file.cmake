bbfe1a9e9ce4bc5404b86da51bf4133a9f63c7e2
Robert Haschke <rhaschke@techfak.uni-bielefeld.de>
2018-08-28 22:07:27 +0200

    atomic_configure_file: lock destination directory
    .. to sync multiple cmake processes writing to that location

--- cmake/atomic_configure_file.cmake
+++ cmake/atomic_configure_file.cmake
@@ -3,5 +3,7 @@ function(atomic_configure_file input output)
   set(atomic_file "${CMAKE_BINARY_DIR}/${atomic_file}")
   configure_file("${input}" "${atomic_file}" ${ARGN})
   get_filename_component(output_path ${output} PATH)
+  # sync multiple catkin cmake processes writing to that location
+  file(LOCK "${output_path}" DIRECTORY GUARD FUNCTION)
   file(COPY "${atomic_file}" DESTINATION "${output_path}")
 endfunction()
