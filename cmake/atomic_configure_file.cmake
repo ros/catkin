57024639630b1dc4040dce85631a18c834acb6cf
Robert Haschke <rhaschke@techfak.uni-bielefeld.de>
2018-09-02 20:26:35 +0200

    create configure files in sub-folder atomic_configure

--- cmake/atomic_configure_file.cmake
+++ cmake/atomic_configure_file.cmake
@@ -1,6 +1,6 @@
 function(atomic_configure_file input output)
   get_filename_component(atomic_file ${output} NAME)
-  set(atomic_file "${CMAKE_BINARY_DIR}/${atomic_file}")
+  set(atomic_file "${CMAKE_BINARY_DIR}/atomic_configure/${atomic_file}")
   configure_file("${input}" "${atomic_file}" ${ARGN})
   get_filename_component(output_path ${output} PATH)
   # sync multiple catkin cmake processes writing to that location
