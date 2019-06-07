7c778a353cf2a2c35bac4e0e79f741722b5ab65a
Sean Yen <seanyen@microsoft.com>
2019-06-07 10:31:25 -0700

    [Windows][kinetic] Add catkin default parameter for Windows developers (#1011)
    * Add win32 default values.
    
    * rename the function to apply_platform_specific_defaults
    
    * no need to return the passed in args since they are being mutated
    
    * style: wrapping block instead of inlining
    
    * simplify logic
    
    * remove duplicate string literal
    
    * remove empty line

--- python/catkin/builder.py
+++ python/catkin/builder.py
@@ -1220,3 +1220,14 @@ def get_package_names_with_recursive_dependencies(packages, pkg_names):
                 if dep in packages_by_name and dep not in check_pkg_names and dep not in dependencies:
                     check_pkg_names.add(dep)
     return dependencies
+
+def apply_platform_specific_defaults(args):
+    # add Windows specific defaults
+    if sys.platform == 'win32':
+        # default to use nmake if on Windows and if not using ninja
+        if not args.use_ninja:
+            args.use_nmake = True
+        # use RelWithDebInfo as default build type if on Windows
+        prefix = '-DCMAKE_BUILD_TYPE='
+        if not any(a.startswith(prefix) for a in args.cmake_args):
+            args.cmake_args.append(prefix + 'RelWithDebInfo')
