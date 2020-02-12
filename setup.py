935fb7400d76e3883b85f5697ce6614ca6e3019f
seanyen <seanyen@microsoft.com>
2020-02-11 23:48:59 -0800

    add more conditional guard.

--- setup.py
+++ setup.py
@@ -3,7 +3,6 @@ from catkin_pkg.python_setup import generate_distutils_setup
 from setuptools import setup
 
 import os
-import shutil
 
 catkin_scripts = [
         'bin/catkin_find',
@@ -15,17 +14,20 @@ catkin_scripts = [
     ]
 
 if os.name == 'nt':
-    # On Windows, Python scripts making use of multiprocessing
-    # have to meet several requirements to work.
-    # (catkin_pkg makes use of it to do parallel package parser.)
-    #
-    # One is that the scripts have to be ended with .py extension;
-    # Otherwise, it will manifest as ImportError.
-    if not os.path.isdir('build/bin'):
-        os.makedirs('build/bin')
-    for script in catkin_scripts:
-        shutil.copyfile(script, 'build/%s.py' % script)
-    catkin_scripts = ['build/%s.py' % script for script in catkin_scripts]
+    import sys
+    import shutil
+    if sys.version_info.major == 2:
+        # On Windows\Python2, Python scripts making use of multiprocessing
+        # have to meet several requirements to work.
+        # (catkin_pkg makes use of it to do parallel package parser.)
+        #
+        # One is that the scripts have to be ended with .py extension;
+        # Otherwise, it will manifest as ImportError.
+        if not os.path.isdir('build/bin'):
+            os.makedirs('build/bin')
+        for script in catkin_scripts:
+            shutil.copyfile(script, 'build/%s.py' % script)
+        catkin_scripts = ['build/%s.py' % script for script in catkin_scripts]
 
 d = generate_distutils_setup(
     packages=['catkin'],
