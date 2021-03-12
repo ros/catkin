03b96477499b43a87ac4c90835bf2aea2e90cae6
Krzysztof Laskowski <krzysztof.laskowski@aeolusbot.com>
2021-03-12 17:20:34 +0000

    catkin pkg config template: performance fix for pack/unpack/append
    These algorithms may be used O(M^2) times where M is the level of
    rospackage nesting, and each pack/unpack/append algorithm complexity
    are O(N^2)/O(N)/O(N^2) where N is libraries count. The fix allows
    to reduce them to O(1) complexity if we only consider amount of cmake
    statements executed.
    
    Notes regarding REMOVE_DUPLICATES in list_append_unique and
    list_append_deduplicate are incorrect, in case of:
    
    - list_append_unique: REMOVE_DUPLICATES uses set/unordered_set to
      remember encountered items, all output items are written in order,
      so the algorithm is "stable", it doesn't shuffle elements
    
    - list_append_deduplicate: REMOVE_DUPLICATES is improper here, as it
      removes duplicates from the added element list, not the added-to element
      list, not due to it's "unstability" as note suggests
    
    3.0.2:  https://github.com/Kitware/CMake/blob/v3.0.2/Source/cmListCommand.cxx#L480-L493
    3.19.6: https://github.com/Kitware/CMake/blob/v3.19.6/Source/cmAlgorithms.h#L120-L130

--- cmake/list_append_deduplicate.cmake
+++ cmake/list_append_deduplicate.cmake
@@ -1,11 +1,6 @@
 #
 # Append elements to a list and remove existing duplicates from the list.
 #
-# .. note:: Using CMake's ``list(APPEND ..)`` and
-#   ``list(REMOVE_DUPLICATES ..)`` is not sufficient since its
-#   implementation uses a set internally which makes the operation
-#   unstable.
-#
 macro(list_append_deduplicate listname)
   if(NOT "${ARGN}" STREQUAL "")
     if(${listname})
